ee9cbb7f93ebc880a59ed516e9e413f05dddb5a7
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-12 18:15:16 +0000

    08061 Rule from AccessiWeb2 implementation

---
+++ AccessiWeb/src/main/java/org/opens/tanaguru/rule/accessiweb2/Aw2Rule08061.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.opens.tanaguru.rule.accessiweb2;
+
+import org.opens.tanaguru.entity.audit.ProcessResult;
+import org.opens.tanaguru.entity.audit.TestSolution;
+import org.opens.tanaguru.entity.reference.Nomenclature;
+import org.opens.tanaguru.processor.SSPHandler;
+import org.opens.tanaguru.ruleimplementation.AbstractPageRuleImplementation;
+
+/**
+ *
+ * @author jkowalczyk
+ */
+public class Aw2Rule08061 extends AbstractPageRuleImplementation {
+
+    private static final String HEAD_TAG = "head";
+    private static final String TTTLE_TAG = "title";
+
+    /**
+     *
+     */
+    public Aw2Rule08061(){
+        super();
+    }
+
+    /**
+     *
+     * @param sspHandler
+     * @return
+     */
+    @Override
+    protected ProcessResult processImpl(SSPHandler sspHandler) {
+
+        sspHandler.beginSelection().selectDocumentNodes(HEAD_TAG).
+                selectChildNodes(TTTLE_TAG);
+
+        Nomenclature unexplicitePageTitle = nomenclatureLoaderService.
+                loadByCode("UnexplicitPageTitle");
+
+        TestSolution testSolution = sspHandler.
+                checkTextContentValue(unexplicitePageTitle.getValueList(), null);
+
+        ProcessResult processResult = definiteResultFactory.create(
+                test,
+                sspHandler.getPage(),
+                testSolution,
+                null);
+
+        return processResult;
+    }
+
+}
