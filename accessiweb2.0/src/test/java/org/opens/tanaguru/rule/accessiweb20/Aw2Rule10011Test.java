5075ec524d0de978fffe449bda5005ef2cb1b095
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-18 10:29:42 +0000

    10011 Rule from AccessiWeb2 implementation

--- accessiweb2.0/src/test/java/org/opens/tanaguru/rule/accessiweb20/Aw2Rule10011Test.java
+++ accessiweb2.0/src/test/java/org/opens/tanaguru/rule/accessiweb20/Aw2Rule10011Test.java
@@ -24,44 +24,84 @@ public class Aw2Rule10011Test extends AbstractRuleImplementationTestCase {
 
     @Override
     protected void setUpWebResourceMap() {
-        webResourceMap.put("AW20.Test.10.01.01-1Passed-01", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-1Passed-01.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-01", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-01.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-02", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-02.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-03", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-03.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-04", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-04.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-05", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-05.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-06", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-06.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-07", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-07.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-08", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-08.html"));
-        webResourceMap.put("AW20.Test.10.01.01-2Failed-09", webResourceFactory.createPage("file:///home/jkowalczyk/Documents/Sources/Test Cases/Testcases repository/AW22/AW20.Test.10.01.01-2Failed-09.html"));
+        webResourceMap.put("AW20.Test.10.01.01-1Passed-01", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-1Passed-01.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-01", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-01.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-02",
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-02.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-03", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-03.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-04", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-04.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-05", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-05.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-06", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-06.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-07", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-07.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-08", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-08.html"));
+        webResourceMap.put("AW20.Test.10.01.01-2Failed-09", 
+                webResourceFactory.createPage(
+                "file:///home/jkowalczyk/Documents/Sources/TanaguruEngine/Trunk/tanaguru-testing-tools/resources/testcases/AW22/AW20.Test.10.01.01-2Failed-09.html"));
     }
 
     @Override
     protected void setProcess() {
-        assertEquals(TestSolution.PASSED, processPageTest("AW20.Test.10.01.01-1Passed-01").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-01").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-02").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-03").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-04").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-05").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-06").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-07").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-08").getValue());
-        assertEquals(TestSolution.FAILED, processPageTest("AW20.Test.10.01.01-2Failed-09").getValue());
+        assertEquals(TestSolution.PASSED,
+                processPageTest("AW20.Test.10.01.01-1Passed-01").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-01").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-02").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-03").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-04").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-05").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-06").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-07").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-08").getValue());
+        assertEquals(TestSolution.FAILED,
+                processPageTest("AW20.Test.10.01.01-2Failed-09").getValue());
     }
 
     @Override
     protected void setConsolidate() {
-        assertEquals(TestSolution.PASSED, consolidate("AW20.Test.10.01.01-1Passed-01").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-01").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-02").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-03").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-04").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-05").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-06").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-07").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-08").getValue());
-        assertEquals(TestSolution.FAILED, consolidate("AW20.Test.10.01.01-2Failed-09").getValue());
+        assertEquals(TestSolution.PASSED,
+                consolidate("AW20.Test.10.01.01-1Passed-01").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-01").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-02").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-03").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-04").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-05").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-06").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-07").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-08").getValue());
+        assertEquals(TestSolution.FAILED,
+                consolidate("AW20.Test.10.01.01-2Failed-09").getValue());
     }
 
 }
