ec0d118a8c00c38174038dad1e144d0843f4696e
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-19 11:37:20 +0000

    01011 Rule from AccessiWeb2 implementation

---
+++ accessiweb2.0/src/main/java/org/opens/tanaguru/rule/accessiweb20/Aw20Rule01011.java
@@ -0,0 +1,38 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.opens.tanaguru.rule.accessiweb20;
+
+import org.opens.tanaguru.entity.audit.DefiniteResult;
+import org.opens.tanaguru.entity.audit.ProcessResult;
+import org.opens.tanaguru.entity.audit.TestSolution;
+import org.opens.tanaguru.processor.SSPHandler;
+import org.opens.tanaguru.ruleimplementation.AbstractPageRuleImplementation;
+
+/**
+ * This rule tests if all the decorative images have an empty "alt" attribute
+ * @author jkowalczyk
+ */
+public class Aw20Rule01011 extends AbstractPageRuleImplementation {
+
+    public static final String IMG_TAG = "IMG";
+    public static final String ALT_ATTRIBUTE = "alt";
+
+    @Override
+    protected ProcessResult processImpl(SSPHandler sspHandler) {
+
+        sspHandler.beginSelection().
+                selectDocumentNodes(IMG_TAG);
+
+        TestSolution checkResult = sspHandler.checkAttributeExists(ALT_ATTRIBUTE);
+
+        DefiniteResult result = definiteResultFactory.create(
+                test,
+                sspHandler.getSSP().getPage(),
+                checkResult,
+                sspHandler.getRemarkList());
+
+        return result;
+    }
+}
