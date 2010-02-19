26e970081dd9e39dcc5b43c2dd0b6be714b30378
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-19 11:44:12 +0000

    Upper_case tags removing

--- tanaguru-testing-tools/src/main/java/org/opens/tanaguru/rule/test/HTMLCorrectorImpl.java
+++ tanaguru-testing-tools/src/main/java/org/opens/tanaguru/rule/test/HTMLCorrectorImpl.java
@@ -31,7 +31,7 @@ public class HTMLCorrectorImpl implements HTMLCleaner {
 
     @Override
     public void run() {
-        result = dirtyHTML;
+        result = removeLowerCaseTags(dirtyHTML);
     }
 
     @Override
@@ -42,5 +42,40 @@ public class HTMLCorrectorImpl implements HTMLCleaner {
     @Override
     public String getCorrectorName() {
         return "Mock";
+    }
+
+     private String removeLowerCaseTags(String cleanHtml) {
+        StringBuffer newCleanHtml = new StringBuffer();
+        int strPtr=0;
+        int tmpPtr=0;
+        while (strPtr != cleanHtml.length()){
+            if (cleanHtml.charAt(strPtr) == '<') {
+                if (cleanHtml.charAt(strPtr+1) == '!') { //To ignore the case of <!doctype
+                    newCleanHtml.append(cleanHtml.charAt(strPtr));
+                    strPtr++;
+                } else if (cleanHtml.charAt(strPtr+1) == '/') {
+                    tmpPtr = cleanHtml.indexOf('>', strPtr);
+                    newCleanHtml.append('<');
+                    newCleanHtml.append('/');
+                    newCleanHtml.append(cleanHtml.substring(strPtr+2, tmpPtr).toLowerCase());
+                    strPtr = tmpPtr;
+                } else {
+                    if (cleanHtml.indexOf(' ', strPtr) < cleanHtml.indexOf('>', strPtr)) {
+                        // case of self-closing tag
+                        tmpPtr = cleanHtml.indexOf(' ', strPtr);
+                    } else {
+                        // case of classical opening tag
+                        tmpPtr = cleanHtml.indexOf('>', strPtr);
+                    }
+                    newCleanHtml.append('<');
+                    newCleanHtml.append(cleanHtml.substring(strPtr+1, tmpPtr).toLowerCase());
+                    strPtr = tmpPtr;
+                }
+            } else {
+                newCleanHtml.append(cleanHtml.charAt(strPtr));
+                strPtr++;
+            }
+        }
+        return newCleanHtml.toString();
     }
 }
