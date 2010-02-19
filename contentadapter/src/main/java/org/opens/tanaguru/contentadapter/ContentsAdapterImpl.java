0f62db6305691f25ccecedbe2b5d9b5340d436f5
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-19 11:41:58 +0000

    01012 Rule from AccessiWeb2 implementation

--- contentadapter/src/main/java/org/opens/tanaguru/contentadapter/ContentsAdapterImpl.java
+++ contentadapter/src/main/java/org/opens/tanaguru/contentadapter/ContentsAdapterImpl.java
@@ -59,7 +59,7 @@ public class ContentsAdapterImpl implements ContentsAdapter {
                 htmlCleaner.setDirtyHTML(ssp.getSource());
                 htmlCleaner.run();
 
-                ssp.setDOM(removeUpperCaseTags(htmlCleaner.getResult()));
+                ssp.setDOM(removeLowerCaseTags(htmlCleaner.getResult()));
                 writeCleanDomInFile(ssp);
 
                 htmlParser.setSSP(ssp);
@@ -97,7 +97,7 @@ public class ContentsAdapterImpl implements ContentsAdapter {
         this.htmlParser = htmlParser;
     }
 
-    private String removeUpperCaseTags(String cleanHtml) {
+    private String removeLowerCaseTags(String cleanHtml) {
         StringBuffer newCleanHtml = new StringBuffer();
         int strPtr=0;
         int tmpPtr=0;
@@ -110,7 +110,7 @@ public class ContentsAdapterImpl implements ContentsAdapter {
                     tmpPtr = cleanHtml.indexOf('>', strPtr);
                     newCleanHtml.append('<');
                     newCleanHtml.append('/');
-                    newCleanHtml.append(cleanHtml.substring(strPtr+2, tmpPtr).toLowerCase());
+                    newCleanHtml.append(cleanHtml.substring(strPtr+2, tmpPtr).toUpperCase());
                     strPtr = tmpPtr;
                 } else {
                     if (cleanHtml.indexOf(' ', strPtr) < cleanHtml.indexOf('>', strPtr)) {
@@ -121,7 +121,7 @@ public class ContentsAdapterImpl implements ContentsAdapter {
                         tmpPtr = cleanHtml.indexOf('>', strPtr);
                     }
                     newCleanHtml.append('<');
-                    newCleanHtml.append(cleanHtml.substring(strPtr+1, tmpPtr).toLowerCase());
+                    newCleanHtml.append(cleanHtml.substring(strPtr+1, tmpPtr).toUpperCase());
                     strPtr = tmpPtr;
                 }
             } else {
