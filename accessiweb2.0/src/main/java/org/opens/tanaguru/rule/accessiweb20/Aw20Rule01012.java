e19812b3d4cab8fa889e37b3a1fea89c61f7fd2b
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-19 13:35:47 +0000

    01012 Rule from AccessiWeb2 implementation

--- accessiweb2.0/src/main/java/org/opens/tanaguru/rule/accessiweb20/Aw20Rule01012.java
+++ accessiweb2.0/src/main/java/org/opens/tanaguru/rule/accessiweb20/Aw20Rule01012.java
@@ -4,19 +4,6 @@
  */
 package org.opens.tanaguru.rule.accessiweb20;
 
-import java.awt.image.BufferedImage;
-import java.io.IOException;
-import javax.imageio.ImageIO;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.w3c.dom.Node;
-import org.opens.tanaguru.contentadapter.util.URLIdentifier;
-import org.opens.tanaguru.contentadapter.util.URLIdentifierImpl;
 import org.opens.tanaguru.entity.audit.DefiniteResult;
 import org.opens.tanaguru.entity.audit.ProcessResult;
 import org.opens.tanaguru.entity.audit.TestSolution;
@@ -24,24 +11,26 @@ import org.opens.tanaguru.processor.SSPHandler;
 import org.opens.tanaguru.ruleimplementation.AbstractPageRuleImplementation;
 
 /**
- * This rule tests if all the decorative images have an empty "alt" attribute
+ * This rule tests if an area tag defined in a map tag, associated with the usemap
+ * attribute of an img tag, has an alt attribute
  * @author jkowalczyk
  */
 public class Aw20Rule01012 extends AbstractPageRuleImplementation {
 
     public static final String XPATH_EXPR = 
-            "//AREA[concat('#', ancestor::MAP@name) = //IMG@usemap]";
+            "//AREA[concat('#', ancestor::MAP/@name) = //IMG/@usemap]";
     public static final String XPATH_EXPR2 =
-            "//AREA[ancestor::MAP@name = //IMG@usemap]";
-    public static final String XPATH_EXPR3 =
-            "//AREA[concat('#', ancestor::MAP@id) = //IMG@usemap]";
-    public static final String XPATH_EXPR4 =
-            "//AREA[ancestor::MAP@id = //IMG@usemap]";
+            "//AREA[ancestor::MAP/@name = //IMG/@usemap]";
 
     public static final String ALT_ATTRIBUTE = "ALT";
 
+    public Aw20Rule01012(){
+        super();
+    }
+
     @Override
     protected ProcessResult processImpl(SSPHandler sspHandler) {
+        
         sspHandler.beginSelection().domXPathSelectNodeSet(XPATH_EXPR);
         TestSolution checkResult = sspHandler.checkAttributeExists(ALT_ATTRIBUTE);
 
@@ -50,14 +39,6 @@ public class Aw20Rule01012 extends AbstractPageRuleImplementation {
             checkResult = sspHandler.checkAttributeExists(ALT_ATTRIBUTE);
         }
         
-        if (checkResult == TestSolution.NOT_APPLICABLE) {
-            sspHandler.beginSelection().domXPathSelectNodeSet(XPATH_EXPR3);
-            checkResult = sspHandler.checkAttributeExists(ALT_ATTRIBUTE);
-        }
-        if (checkResult == TestSolution.NOT_APPLICABLE) {
-            sspHandler.beginSelection().domXPathSelectNodeSet(XPATH_EXPR4);
-            checkResult = sspHandler.checkAttributeExists(ALT_ATTRIBUTE);
-        }
         DefiniteResult result = definiteResultFactory.create(
                 test,
                 sspHandler.getSSP().getPage(),
