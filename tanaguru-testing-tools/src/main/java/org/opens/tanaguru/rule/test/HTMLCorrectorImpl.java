91bb4c4706a5719be8e4ccc3431fa0324fa5e343
Jérôme Kowalczyk <jk@tanaguru.com>
2010-02-22 09:59:36 +0000

    Upper_case tags removing

--- tanaguru-testing-tools/src/main/java/org/opens/tanaguru/rule/test/HTMLCorrectorImpl.java
+++ tanaguru-testing-tools/src/main/java/org/opens/tanaguru/rule/test/HTMLCorrectorImpl.java
@@ -5,6 +5,7 @@
 package org.opens.tanaguru.rule.test;
 
 import org.opens.tanaguru.contentadapter.HTMLCleaner;
+import org.opens.tanaguru.contentadapter.util.DocumentCaseInsensitiveAdapter;
 
 /**
  *
@@ -31,7 +32,7 @@ public class HTMLCorrectorImpl implements HTMLCleaner {
 
     @Override
     public void run() {
-        result = removeLowerCaseTags(dirtyHTML);
+        result = DocumentCaseInsensitiveAdapter.removeLowerCaseTags(dirtyHTML);
     }
 
     @Override
@@ -44,38 +45,4 @@ public class HTMLCorrectorImpl implements HTMLCleaner {
         return "Mock";
     }
 
-     private String removeLowerCaseTags(String cleanHtml) {
-        StringBuffer newCleanHtml = new StringBuffer();
-        int strPtr=0;
-        int tmpPtr=0;
-        while (strPtr != cleanHtml.length()){
-            if (cleanHtml.charAt(strPtr) == '<') {
-                if (cleanHtml.charAt(strPtr+1) == '!') { //To ignore the case of <!doctype
-                    newCleanHtml.append(cleanHtml.charAt(strPtr));
-                    strPtr++;
-                } else if (cleanHtml.charAt(strPtr+1) == '/') {
-                    tmpPtr = cleanHtml.indexOf('>', strPtr);
-                    newCleanHtml.append('<');
-                    newCleanHtml.append('/');
-                    newCleanHtml.append(cleanHtml.substring(strPtr+2, tmpPtr).toLowerCase());
-                    strPtr = tmpPtr;
-                } else {
-                    if (cleanHtml.indexOf(' ', strPtr) < cleanHtml.indexOf('>', strPtr)) {
-                        // case of self-closing tag
-                        tmpPtr = cleanHtml.indexOf(' ', strPtr);
-                    } else {
-                        // case of classical opening tag
-                        tmpPtr = cleanHtml.indexOf('>', strPtr);
-                    }
-                    newCleanHtml.append('<');
-                    newCleanHtml.append(cleanHtml.substring(strPtr+1, tmpPtr).toLowerCase());
-                    strPtr = tmpPtr;
-                }
-            } else {
-                newCleanHtml.append(cleanHtml.charAt(strPtr));
-                strPtr++;
-            }
-        }
-        return newCleanHtml.toString();
-    }
 }
