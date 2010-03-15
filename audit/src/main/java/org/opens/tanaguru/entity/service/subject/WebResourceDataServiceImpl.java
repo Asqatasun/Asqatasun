a28ee0e3e550933e60d79771caa9c27b006f99fd
Jérôme Kowalczyk <jk@tanaguru.com>
2010-03-15 16:52:17 +0000

    Bug #31 correction

--- audit/src/main/java/org/opens/tanaguru/entity/service/subject/WebResourceDataServiceImpl.java
+++ audit/src/main/java/org/opens/tanaguru/entity/service/subject/WebResourceDataServiceImpl.java
@@ -1,6 +1,5 @@
 package org.opens.tanaguru.entity.service.subject;
 
-import org.opens.tanaguru.entity.service.subject.*;
 import org.opens.tanaguru.entity.subject.Page;
 import org.opens.tanaguru.entity.subject.Site;
 import org.opens.tanaguru.entity.subject.WebResource;
@@ -12,23 +11,55 @@ import com.adex.sdk.entity.service.AbstractGenericDataService;
  * 
  * @author ADEX
  */
-public class WebResourceDataServiceImpl extends
-		AbstractGenericDataService<WebResource, Long> implements
-		WebResourceDataService {
+public class WebResourceDataServiceImpl extends AbstractGenericDataService<WebResource, Long> implements
+        WebResourceDataService {
 
-	public WebResourceDataServiceImpl() {
-		super();
-	}
+    private final String HTTP_PROTOCOL_PREFIX = "http://";
+    private final String HTTPS_PROTOCOL_PREFIX = "https://";
+    private final String FILE_PROTOCOL_PREFIX = "file://";
+    private final String FILE_PREFIX = "/";
+//    private final String windowsPathPattern = " ^([a-zA-Z]:) \\{1} | ((\\{1}) [^\\] ([^/:*?&lt;&gt;&quot;|]*(?&lt;![ ])))+)$" ;
 
-	public Page createPage(String url) {
-		return ((WebResourceFactory) entityFactory).createPage(url);
-	}
+    public WebResourceDataServiceImpl() {
+        super();
+    }
 
-	public Site createSite(String url) {
-		return ((WebResourceFactory) entityFactory).createSite(url);
-	}
+    @Override
+    public Page createPage(String url) {
+        return ((WebResourceFactory) entityFactory).createPage(addProtocolToUrl(url));
+    }
 
-	public WebResource findByUrl(String url) {
-		return ((WebResourceDAO) entityDao).findByUrl(url);
-	}
+    @Override
+    public Site createSite(String url) {
+        return ((WebResourceFactory) entityFactory).createSite(url);
+    }
+
+    @Override
+    public WebResource findByUrl(String url) {
+        return ((WebResourceDAO) entityDao).findByUrl(url);
+    }
+
+    /**
+     * This method add the protocol to the url if the protocol is missing
+     * @param url
+     * @return
+     */
+    private String addProtocolToUrl(String url) {
+//        Pattern p = Pattern.compile(windowsPathPattern);
+//        Matcher m = p.matcher(url);
+
+        if (!url.startsWith(HTTP_PROTOCOL_PREFIX)
+                && !url.startsWith(HTTPS_PROTOCOL_PREFIX)
+                && !url.startsWith(FILE_PROTOCOL_PREFIX)) {
+
+            if (url.startsWith(FILE_PREFIX)) {
+//                    m.find()) {
+                url = FILE_PROTOCOL_PREFIX + url;
+            } else {
+                url = HTTP_PROTOCOL_PREFIX + url;
+            }
+
+        }
+        return url;
+    }
 }
