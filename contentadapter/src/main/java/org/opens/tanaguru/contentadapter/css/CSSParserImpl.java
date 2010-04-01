4904f46b239a855bef8dbefd3d27a552f8fd689f
Jérôme Kowalczyk <jk@tanaguru.com>
2010-04-01 16:29:17 +0000

    10041 Rule from AccessiWeb2 implementation

--- contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSParserImpl.java
+++ contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSParserImpl.java
@@ -10,6 +10,7 @@ import java.util.logging.Logger;
 import org.w3c.css.sac.CSSException;
 import org.w3c.css.sac.InputSource;
 import org.w3c.css.sac.Parser;
+import org.w3c.flute.parser.TokenMgrError;
 
 /**
  * 
@@ -46,11 +47,15 @@ public class CSSParserImpl extends AbstractContentParser implements CSSParser {
         } catch (CSSException ex) {
             Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
                     null, ex);
-            throw new RuntimeException(ex);
         } catch (IOException ex) {
             Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
                     null, ex);
-            throw new RuntimeException(ex);
+        } catch (StringIndexOutOfBoundsException ex ) {
+            Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
+                    null, ex);
+        } catch (TokenMgrError err) {
+            Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
+                    null, err);
         }
     }
 
@@ -75,11 +80,19 @@ public class CSSParserImpl extends AbstractContentParser implements CSSParser {
         } catch (CSSException ex) {
             Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
                     null, ex);
-            throw new RuntimeException(ex);
+            return null;
         } catch (IOException ex) {
             Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
                     null, ex);
-            throw new RuntimeException(ex);
+            return null;
+        }  catch (StringIndexOutOfBoundsException ex ) {
+            Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
+                    null, ex);
+            return null;
+        } catch (TokenMgrError err) {
+            Logger.getLogger(CSSParserImpl.class.getName()).log(Level.SEVERE,
+                    null, err);
+            return null;
         }
     }
 
