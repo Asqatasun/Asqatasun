49bd3bcd724d3c54de495fcef5626a855e52f653
Jérôme Kowalczyk <jk@tanaguru.com>
2010-04-01 10:22:27 +0000

    10041 Rule from AccessiWeb2 implementation

--- contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSResource.java
+++ contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSResource.java
@@ -1,6 +1,18 @@
 package org.opens.tanaguru.contentadapter.css;
 
 import org.opens.tanaguru.contentadapter.Resource;
+import org.w3c.css.sac.SACMediaList;
 
 public interface CSSResource extends Resource {
+    /**
+     *
+     * @return the Resource type
+     */
+    SACMediaList getCssMediaList();
+
+    /**
+     *
+     * @return the Resource type
+     */
+    void setCssMediaList(SACMediaList media);
 }
--- contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSResourceImpl.java
+++ contentadapter/src/main/java/org/opens/tanaguru/contentadapter/css/CSSResourceImpl.java
@@ -2,22 +2,56 @@ package org.opens.tanaguru.contentadapter.css;
 
 import org.opens.tanaguru.contentadapter.util.AbstractResource;
 import org.opens.tanaguru.contentadapter.RsrcLocator;
+import org.w3c.css.sac.SACMediaList;
 
 public class CSSResourceImpl extends AbstractResource implements CSSResource {
 
-	public CSSResourceImpl() {
-		super();
-	}
+    private SACMediaList mediaList;
+    private final String resourceName = "#StyleSheet";
+    private final String defaultMediaValue = "all";
+    private final int resourceType = 1;
 
-	public CSSResourceImpl(String resource, int lineNumber, RsrcLocator location) {
-		super(resource, lineNumber, location);
-	}
+    public CSSResourceImpl() {
+        super();
+    }
 
-	public String getRsrcName() {
-		return "#StyleSheet";
-	}
+    public CSSResourceImpl(
+            String resource,
+            int lineNumber,
+            RsrcLocator location) {
 
-	public short getRsrcType() {
-		return 1;
-	}
+        super(resource, lineNumber, location);
+        mediaList = new SACMediaListImpl();
+        ((SACMediaListImpl) mediaList).addItem(defaultMediaValue);
+    }
+
+    public CSSResourceImpl(
+            String resource,
+            int lineNumber,
+            RsrcLocator location,
+            SACMediaList mediaList) {
+
+        super(resource, lineNumber, location);
+        this.setCssMediaList(mediaList);
+    }
+
+    @Override
+    public String getRsrcName() {
+        return resourceName;
+    }
+
+    @Override
+    public short getRsrcType() {
+        return resourceType;
+    }
+
+    @Override
+    public SACMediaList getCssMediaList() {
+        return mediaList;
+    }
+
+    @Override
+    public void setCssMediaList(SACMediaList mediaList) {
+        this.mediaList = mediaList;
+    }
 }
