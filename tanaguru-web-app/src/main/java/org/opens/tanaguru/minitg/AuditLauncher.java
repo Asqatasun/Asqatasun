bdd5529fea448b56226bef0819d176a89fb8d44d
Jérôme Kowalczyk <jk@tanaguru.com>
2010-04-08 19:29:35 +0000

    Synchronization of the processRequest method on the servlet

--- tanaguru-web-app/src/main/java/org/opens/tanaguru/minitg/AuditLauncher.java
+++ tanaguru-web-app/src/main/java/org/opens/tanaguru/minitg/AuditLauncher.java
@@ -47,7 +47,7 @@ public class AuditLauncher extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+    synchronized protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
