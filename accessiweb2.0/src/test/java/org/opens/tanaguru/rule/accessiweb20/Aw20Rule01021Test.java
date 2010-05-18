56512a78fd48539d4e1669a1824c2916dc9d7bf3
Jérôme Kowalczyk <jk@tanaguru.com>
2010-05-18 17:23:54 +0000

    Feature #64 Implementation of the crawler component based on heritrix

--- accessiweb2.0/src/test/java/org/opens/tanaguru/rule/accessiweb20/Aw20Rule01021Test.java
+++ accessiweb2.0/src/test/java/org/opens/tanaguru/rule/accessiweb20/Aw20Rule01021Test.java
@@ -4,6 +4,8 @@
  */
 package org.opens.tanaguru.rule.accessiweb20;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.opens.tanaguru.entity.audit.TestSolution;
 import org.opens.tanaguru.rule.test.AbstractRuleImplementationTestCase;
 
@@ -61,6 +63,57 @@ public class Aw20Rule01021Test extends AbstractRuleImplementationTestCase {
         webResourceMap.put("AW20.Test.01.02.01-4NA-03",
                 webResourceFactory.createPage(
                 testcasesFilePath + "AW22/AW20.Test.01.02.01-4NA-03.html"));
+
+        setUpRelatedContentMap();
+    }
+
+    private void setUpRelatedContentMap(){
+
+        List<String> relatedContent1 = new ArrayList<String>();
+        relatedContent1.add("images/decorative-black.jpg");
+        relatedContent1.add("images/decorative-white.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-1Passed-01"), relatedContent1);
+
+        List<String> relatedContent2 = new ArrayList<String>();
+        relatedContent2.add("images/mono-dimension-heigth.jpg");
+        relatedContent2.add("images/mono-dimension-width.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-1Passed-02"), relatedContent2);
+
+        List<String> relatedContent3 = new ArrayList<String>();
+        relatedContent3.add("images/decorative-black.jpg");
+        relatedContent3.add("images/decorative-red.jpg");
+        relatedContent3.add("images/informative1.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-1Passed-03"), relatedContent3);
+
+        List<String> relatedContent4 = new ArrayList<String>();
+        relatedContent4.add("images/decorative-black.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-1Passed-04"), relatedContent4);
+
+        List<String> relatedContent5 = new ArrayList<String>();
+        relatedContent5.add("images/decorative-black.jpg");
+        relatedContent5.add("images/decorative-red.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-2Failed-01"), relatedContent5);
+
+        List<String> relatedContent6 = new ArrayList<String>();
+        relatedContent6.add("images/mono-dimension-heigth.jpg");
+        relatedContent6.add("images/mono-dimension-width.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-2Failed-02"), relatedContent6);
+
+        List<String> relatedContent7 = new ArrayList<String>();
+        relatedContent7.add("images/mono-dimension-heigth.jpg");
+        relatedContent7.add("images/mono-dimension-width.jpg");
+        relatedContent7.add("http://www.tanaguru.org/sites/default/files/pictures/picture-1.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-2Failed-03"), relatedContent7);
+
+        List<String> relatedContent8 = new ArrayList<String>();
+        relatedContent8.add("images/decorative-black.jpg");
+        relatedContent8.add("images/informative1.jpg");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-3NMI-01"), relatedContent8);
+
+        List<String> relatedContent9 = new ArrayList<String>();
+        relatedContent9.add("images/informative1.jpg");
+        relatedContent9.add("images/informative2.gif");
+        relatedContentMap.put(webResourceMap.get("AW20.Test.01.02.01-3NMI-02"), relatedContent9);
     }
 
     @Override
