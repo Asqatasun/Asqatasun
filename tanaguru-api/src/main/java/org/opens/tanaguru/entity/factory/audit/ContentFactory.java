453cae40b46518ff0c41abf11a4056489492a721
Jérôme Kowalczyk <jk@tanaguru.com>
2010-05-18 17:06:06 +0000

    Feature #64 Implementation of the crawler component based on heritrix

--- tanaguru-api/src/main/java/org/opens/tanaguru/entity/factory/audit/ContentFactory.java
+++ tanaguru-api/src/main/java/org/opens/tanaguru/entity/factory/audit/ContentFactory.java
@@ -1,11 +1,14 @@
 package org.opens.tanaguru.entity.factory.audit;
 
-import org.opens.tanaguru.entity.audit.Content;
-import org.opens.tanaguru.entity.audit.SSP;
-import org.opens.tanaguru.entity.subject.Page;
+
 import com.adex.sdk.entity.factory.GenericFactory;
 import java.util.Date;
+
+import org.opens.tanaguru.entity.audit.Content;
+import org.opens.tanaguru.entity.audit.ImageContent;
 import org.opens.tanaguru.entity.audit.JavascriptContent;
+import org.opens.tanaguru.entity.subject.Page;
+import org.opens.tanaguru.entity.audit.SSP;
 import org.opens.tanaguru.entity.audit.StylesheetContent;
 
 /**
@@ -18,7 +21,19 @@ public interface ContentFactory extends GenericFactory<Content> {
 
     SSP createSSP(Date dateOfLoading, String uri, String sourceCode, Page page);
 
+    SSP createSSP(Date dateOfLoading, String uri, int httpStatusCode);
+
+    SSP createSSP(Date dateOfLoading, String uri, String sourceCode, Page page, int httpStatusCode);
+
     StylesheetContent createStylesheetContent(Date dateOfLoading, String uri, SSP ssp, String sourceCode);
 
+    StylesheetContent createStylesheetContent(Date dateOfLoading, String uri, SSP ssp, String sourceCode, int httpStatusCode);
+
     JavascriptContent createJavascriptContent(Date dateOfLoading, String uri, SSP ssp, String sourceCode);
+
+    JavascriptContent createJavascriptContent(Date dateOfLoading, String uri, SSP ssp, String sourceCode, int httpStatusCode);
+
+    ImageContent createImageContent(Date dateOfLoading, String uri, SSP ssp, byte[] binaryContent);
+
+    ImageContent createImageContent(Date dateOfLoading, String uri, SSP ssp, byte[] binaryContent, int httpStatusCode);
 }
