5b9710b7145cd9323ac5b6d7cce0dc6aed76b7b9
Jérôme Kowalczyk <jk@tanaguru.com>
2011-03-22 10:46:47 +0000

    API : MD5 encoder utility class

---
+++ tanaguru-api/src/main/java/org/opens/tanaguru/util/MD5Encoder.java
@@ -0,0 +1,44 @@
+package org.opens.tanaguru.util;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author jkowalczyk
+ */
+public class MD5Encoder {
+
+    /**
+     * Default private constructor
+     */
+    private MD5Encoder() {}
+
+    private static String convertToHex(byte[] data) {
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < data.length; i++) {
+            int halfbyte = (data[i] >>> 4) & 0x0F;
+            int two_halfs = 0;
+            do {
+                if ((0 <= halfbyte) && (halfbyte <= 9)) {
+                    buf.append((char) ('0' + halfbyte));
+                } else {
+                    buf.append((char) ('a' + (halfbyte - 10)));
+                }
+                halfbyte = data[i] & 0x0F;
+            } while (two_halfs++ < 1);
+        }
+        String md5 = buf.toString();
+        buf.delete(0, buf.length()-1);
+        return md5;
+    }
+
+    public static String MD5(String text)
+            throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md;
+        md = MessageDigest.getInstance("MD5");
+        md.update(text.getBytes("UTF-8"), 0, text.length());
+        return convertToHex(md.digest());
+    }
+}
