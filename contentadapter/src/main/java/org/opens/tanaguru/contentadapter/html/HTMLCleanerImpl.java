b8ccfa208ba15455add8b65a58ea5357819db232
Jérôme Kowalczyk <jk@tanaguru.com>
2011-06-15 16:58:11 +0000

    HTMLCleanerImpl : keep xml:lang declaration to html node

--- contentadapter/src/main/java/org/opens/tanaguru/contentadapter/html/HTMLCleanerImpl.java
+++ contentadapter/src/main/java/org/opens/tanaguru/contentadapter/html/HTMLCleanerImpl.java
@@ -14,9 +14,7 @@ import org.opens.tanaguru.contentadapter.HTMLCleaner;
 
 public class HTMLCleanerImpl extends AbstractHTMLCleaner implements HTMLCleaner {
     static final String CORRECTOR_NAME = "HTMLCleaner";
-    private static final String XML_ATTRIBUTE = "xmlns:xml";
-    private static final String LANG_ATTRIBUTE = "xml:lang";
-    private HtmlCleaner cleaner;
+        private HtmlCleaner cleaner;
     private CleanerProperties props;
     private XmlSerializer serializer;
 
@@ -25,38 +23,17 @@ public class HTMLCleanerImpl extends AbstractHTMLCleaner implements HTMLCleaner 
         cleaner = new HtmlCleaner();
         props = cleaner.getProperties();
         props.setOmitComments(true);
-        props.setOmitDoctypeDeclaration(false);
+        props.setOmitXmlDeclaration(true);
+        props.setOmitDoctypeDeclaration(true);
         props.setUseCdataForScriptAndStyle(true);
+        props.setNamespacesAware(true);
         serializer = new PrettyXmlSerializer(props);
     }
 
     @Override
     public void run() {
         try {
-//            HtmlCleaner cleaner = new HtmlCleaner();
-//            CleanerProperties props = cleaner.getProperties();
-//            props.setAdvancedXmlEscape(true);
-//            props.setAllowHtmlInsideAttributes(false);
-//            props.setAllowMultiWordAttributes(true);
-//            props.setIgnoreQuestAndExclam(true);
-//            props.setNamespacesAware(true);
-//            props.setRecognizeUnicodeChars(true);
-//            props.setTranslateSpecialEntities(false);
-//            props.setTreatDeprecatedTagsAsContent(false);
-//            props.setTreatUnknownTagsAsContent(false);
-//            props.setUseEmptyElementTags(true);
-//            props.setOmitDeprecatedTags(false);
-//            props.setOmitHtmlEnvelope(false);
-//            props.setOmitUnknownTags(false);
-//            props.setOmitXmlDeclaration(false);
-//            props.setOmitComments(true);
-//            props.setOmitDoctypeDeclaration(false);
-//            props.setUseCdataForScriptAndStyle(true);
-            // to avoid SAX Fatal Error related to namespace
             TagNode node = cleaner.clean(dirtyHTML);
-            node.removeAttribute(LANG_ATTRIBUTE);
-            node.removeAttribute(XML_ATTRIBUTE);
-//            serializer = new PrettyXmlSerializer(props);
             result = serializer.getAsString(node);
             node=null;
         } catch (IOException ex) {
