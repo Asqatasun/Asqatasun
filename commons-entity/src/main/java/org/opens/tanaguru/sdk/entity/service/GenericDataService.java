d2fbdd3773d1543c75e13bc71621c4e203b73f50
Jérôme Kowalczyk <jk@tanaguru.com>
2011-11-02 16:12:51 +0000

    Engine : modify packaging of commons-entity projects from com.adex.sdk to org.opens.tanaguru.sdk

---
+++ commons-entity/src/main/java/org/opens/tanaguru/sdk/entity/service/GenericDataService.java
@@ -0,0 +1,122 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2011  Open-S Company
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
+package org.opens.tanaguru.sdk.entity.service;
+
+import org.opens.tanaguru.sdk.entity.Entity;
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.opens.tanaguru.sdk.entity.factory.GenericFactory;
+import org.opens.tanaguru.sdk.entity.dao.GenericDAO;
+import java.util.Set;
+
+/**
+ * 
+ * @param <E>
+ * @param <K>
+ * @author ADEX
+ * @version 1.0.0
+ */
+public interface GenericDataService<E extends Entity, K extends Serializable> {
+
+    /**
+     *
+     * @return the entity created
+     */
+    E create();
+
+    /**
+     *
+     * @param entity
+     *            the entity to create
+     */
+    void create(E entity);
+
+    /**
+     *
+     * @param entity
+     *            the entity to delete
+     */
+    void delete(E entity);
+
+    /**
+     *
+     * @param key
+     *            the key of the entity to delete
+     */
+    void delete(K key);
+
+    /**
+     * 
+     * @param entitySet
+     */
+    void delete(Set<E> entitySet);
+
+    /**
+     *
+     * @return all instancies of the entity
+     */
+    Collection<? extends E> findAll();
+
+    /**
+     *
+     * @param key
+     *            the key of the entity to read
+     * @return the entity read
+     */
+    E read(K key);
+
+    /**
+     *
+     * @param entity
+     *            the entity to update
+     * @return the entity updated
+     */
+    E saveOrUpdate(E entity);
+
+    /**
+     * 
+     * @param entitySet
+     * @return
+     */
+    Set<E> saveOrUpdate(Set<E> entitySet);
+
+    /**
+     *
+     * @param dao
+     *            the DAO to set
+     */
+    void setEntityDao(GenericDAO<E, K> dao);
+
+    /**
+     *
+     * @param factory
+     *            the factory to set
+     */
+    void setEntityFactory(GenericFactory<E> factory);
+
+    /**
+     *
+     * @param entity
+     *            the entity to update
+     * @return the entity updated
+     */
+    E update(E entity);
+}
