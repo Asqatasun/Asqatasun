485126946e4bff9baa2cf126879f051d9eff6dd4
Jérôme Kowalczyk <jk@tanaguru.com>
2011-11-02 18:01:54 +0000

    Engine : add licence headings to audit project and add basis of javadoc

--- audit/src/main/java/org/opens/tanaguru/entity/service/subject/WebResourceDataServiceImpl.java
+++ audit/src/main/java/org/opens/tanaguru/entity/service/subject/WebResourceDataServiceImpl.java
@@ -1,3 +1,24 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2011  Open-S Company
+ *
+ * This file is part of Tanaguru.
+ *
+ * Tanaguru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
 package org.opens.tanaguru.entity.service.subject;
 
 import java.util.List;
@@ -6,13 +27,13 @@ import org.opens.tanaguru.entity.subject.Site;
 import org.opens.tanaguru.entity.subject.WebResource;
 import org.opens.tanaguru.entity.factory.subject.WebResourceFactory;
 import org.opens.tanaguru.entity.dao.subject.WebResourceDAO;
-import com.adex.sdk.entity.service.AbstractGenericDataService;
+import org.opens.tanaguru.sdk.entity.service.AbstractGenericDataService;
 import org.opens.tanaguru.entity.audit.ProcessResult;
 import org.opens.tanaguru.util.FileNaming;
 
 /**
  * 
- * @author ADEX
+ * @author jkowalczyk
  */
 public class WebResourceDataServiceImpl extends AbstractGenericDataService<WebResource, Long> implements
         WebResourceDataService {
--- audit/src/main/java/org/opens/tanaguru/entity/subject/WebResourceImpl.java
+++ audit/src/main/java/org/opens/tanaguru/entity/subject/WebResourceImpl.java
@@ -1,3 +1,24 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2011  Open-S Company
+ *
+ * This file is part of Tanaguru.
+ *
+ * Tanaguru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
 package org.opens.tanaguru.entity.subject;
 
 import org.opens.tanaguru.entity.audit.Audit;
@@ -23,6 +44,10 @@ import javax.xml.bind.annotation.XmlTransient;
 import org.opens.tanaguru.entity.audit.ProcessResult;
 import org.opens.tanaguru.entity.audit.ProcessResultImpl;
 
+/**
+ * 
+ * @author jkowalczyk
+ */
 @Entity
 @Table(name = "WEB_RESOURCE")
 public abstract class WebResourceImpl implements WebResource, Serializable {
