8b88046c34305618f81e6cbeada20f9ed281b2ee
Jérôme Kowalczyk <jk@tanaguru.com>
2011-11-02 18:40:23 +0000

    Engine : add licence headings to reference project and add basis of javadoc

--- reference/src/main/java/org/opens/tanaguru/entity/factory/reference/NomenclatureElementFactoryImpl.java
+++ reference/src/main/java/org/opens/tanaguru/entity/factory/reference/NomenclatureElementFactoryImpl.java
@@ -1,3 +1,24 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2011  Open-S Company
+ *
+ * This file is part of Tanaguru.
+ *
+ * Tanaguru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
 package org.opens.tanaguru.entity.factory.reference;
 
 import org.opens.tanaguru.entity.reference.NomenclatureElement;
@@ -5,20 +26,21 @@ import org.opens.tanaguru.entity.reference.NomenclatureElementImpl;
 
 /**
  * 
- * @author ADEX
+ * @author jkowalczyk
  */
 public class NomenclatureElementFactoryImpl implements
-		NomenclatureElementFactory {
+        NomenclatureElementFactory {
 
-	public NomenclatureElementFactoryImpl() {
-		super();
-	}
+    public NomenclatureElementFactoryImpl() {
+        super();
+    }
 
-	public NomenclatureElement create() {
-		return new NomenclatureElementImpl();
-	}
+    public NomenclatureElement create() {
+        return new NomenclatureElementImpl();
+    }
 
-	public NomenclatureElement create(String value) {
-		return new NomenclatureElementImpl(value);
-	}
+    public NomenclatureElement create(String value) {
+        return new NomenclatureElementImpl(value);
+    }
+
 }
--- reference/src/main/java/org/opens/tanaguru/entity/reference/ThemeImpl.java
+++ reference/src/main/java/org/opens/tanaguru/entity/reference/ThemeImpl.java
@@ -1,3 +1,24 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2011  Open-S Company
+ *
+ * This file is part of Tanaguru.
+ *
+ * Tanaguru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
 package org.opens.tanaguru.entity.reference;
 
 import java.io.Serializable;
@@ -15,6 +36,10 @@ import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlRootElement;
 
+/**
+ * 
+ * @author jkowalczyk
+ */
 @Entity
 @Table(name = "THEME")
 @XmlRootElement
