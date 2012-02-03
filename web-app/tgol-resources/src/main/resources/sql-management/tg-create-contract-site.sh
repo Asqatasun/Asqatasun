e6fc49605ecfc695eed0e5077416519358c77969
Jérôme Kowalczyk <jk@tanaguru.com>
2012-02-03 10:40:39 +0000

    contract management

---
+++ web-app/tgol-resources/src/main/resources/sql-management/tg-create-contract-site.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 20120201 mfaure
+
+# Db authentication
+DbUser=
+DbUserPasswd=
+DbName=
+
+# For Getopts: very first ":" for error managing, then a ":" after each option requiring an argument
+while getopts ":l:w:u:" opt; do
+  case $opt in
+    l) SiteLabel="$OPTARG" ;;
+    w) URL="$OPTARG" ;;
+    u) UserId="$OPTARG" ;;
+    :) echo "Missing argument for option -$OPTARG" ;;
+    ?) echo "Unkown option $OPTARG" ;;
+  esac
+done
+
+if [ -z "$UserId" ]; then
+        echo "Usage $0 -u <UserId> "
+        exit -1
+fi
+
+mysql -u $DbUser -p$DbUserPasswd $DbName -e "
+use tanaguru;
+insert into TGSI_CONTRACT (Label, Url, Begin_Date, End_Date, USER_Id_User, PRODUCT_Id_Product) values (\"$SiteLabel\", \"$URL\", date(now()), date_add(date(now()), interval 1 year), \"$UserId\", \"2\");"
