ca55c52d14261474664e7309cf957990d624603a
Jérôme Kowalczyk <jk@tanaguru.com>
2012-05-04 17:54:08 +0000

    tgol-resources : add contract management procedure and script

---
+++ web-app/tgol-resources/src/main/resources/sql-management/create-contract-procedure.sql
@@ -0,0 +1,65 @@
+-- -----------------------------------------------------------------
+-- Creation of contract with Url, Functionalities and Referentials
+-- -----------------------------------------------------------------
+delimiter |
+CREATE DEFINER=`tanaguru`@`localhost` PROCEDURE `create_contract`(
+IN idUser INT, 
+IN label VARCHAR(255), 
+IN url VARCHAR(1024), 
+IN ref1 INT, 
+IN ref2 INT, 
+IN funct1 INT, 
+IN funct2 INT, 
+IN funct3 INT)
+BEGIN
+
+  DECLARE v_Id_Option_Element bigint(20);
+  DECLARE contractId bigint(20);
+
+  INSERT INTO TGSI_CONTRACT (Label, Begin_Date, End_Date, USER_Id_User) values (label, date(now()),
+                date_add(date(now()), interval 1 year), idUser);
+
+  SELECT LAST_INSERT_ID() INTO contractId; 
+
+  IF url IS NOT NULL AND LENGTH(url)>0 
+    THEN 
+        INSERT IGNORE INTO `TGSI_OPTION_ELEMENT` (`OPTION_Id_Option`, `Value`) VALUES
+            (10, url);	
+        select Id_Option_Element into v_Id_Option_Element FROM TGSI_OPTION_ELEMENT toe WHERE toe.OPTION_Id_Option=10 AND toe.Value=Url;
+        INSERT IGNORE INTO `TGSI_CONTRACT_OPTION_ELEMENT` (`CONTRACT_Id_Contract`, `OPTION_ELEMENT_Id_Option_Element`) VALUES
+	    (contractId,v_Id_Option_Element);
+  END IF;
+
+  IF ref1 IS NOT NULL 
+    THEN 
+        INSERT IGNORE INTO `TGSI_CONTRACT_REFERENTIAL` (`CONTRACT_Id_Contract`, `REFERENTIAL_Id_Referential`) VALUES
+	    (contractId,1);
+  END IF;            
+
+  IF ref2 IS NOT NULL 
+    THEN 
+        INSERT IGNORE INTO `TGSI_CONTRACT_REFERENTIAL` (`CONTRACT_Id_Contract`, `REFERENTIAL_Id_Referential`) VALUES
+	    (contractId,2);
+  END IF;
+
+  IF funct1 IS NOT NULL 
+    THEN 
+        INSERT IGNORE INTO `TGSI_CONTRACT_FUNCTIONALITY` (`CONTRACT_Id_Contract`, `FUNCTIONALITY_Id_Functionality`) VALUES
+	    (contractId,funct1);
+  END IF;
+
+  IF funct2 IS NOT NULL 
+    THEN 
+        INSERT IGNORE INTO `TGSI_CONTRACT_FUNCTIONALITY` (`CONTRACT_Id_Contract`, `FUNCTIONALITY_Id_Functionality`) VALUES
+	    (contractId,funct2);
+  END IF;
+
+  IF funct3 IS NOT NULL 
+    THEN 
+        INSERT IGNORE INTO `TGSI_CONTRACT_FUNCTIONALITY` (`CONTRACT_Id_Contract`, `FUNCTIONALITY_Id_Functionality`) VALUES
+	    (contractId,funct3);
+  END IF;
+    
+END  |
+delimiter ;
+
--- web-app/tgol-resources/src/main/resources/sql-management/tg-create-contract.sh
+++ web-app/tgol-resources/src/main/resources/sql-management/tg-create-contract.sh
@@ -3,30 +3,76 @@
 # 20111102 mfaure
 
 # For Getopts: very first ":" for error managing, then a ":" after each option requiring an argument
-DbUser=
-DbUserPasswd=
-DbName=
+DbUser=tanaguru
+DbUserPasswd=t4n4guru
+DbName=tanaguruProd
 
-while getopts ":l:w:u:p:" opt; do
+while getopts ":l:w:u:f:r:" opt; do
   case $opt in
     l) SiteLabel="$OPTARG" ;;
     w) URL="$OPTARG" ;;
     u) UserId="$OPTARG" ;;
-    p) ProductId="$OPTARG" ;;
+    f) Functs="$OPTARG" ;;
+    r) Refs="$OPTARG" ;;
     :) echo "Missing argument for option -$OPTARG" ;;
     ?) echo "Unkown option $OPTARG" ;;
   esac
 done
 
-if [ -z "$SiteLabel" ] || [ -z "$URL" ] || [ -z "$UserId" ] || [ -z "$ProductId" ]; then
-	echo "Usage $0 -l <SiteLabel> -w <FQDN-url> -u <UserId> -p <ProductId> "
-        echo "ProductId=1 -> Up to 10 pages Audit"
-        echo "ProductId=2 -> Site Audit "
-        echo "ProductId=7 -> File upload Audit "
+echo $SiteLabel 
+echo $URL
+echo $Functs 
+echo $Refs
+echo "stop"
+if [ -z "$SiteLabel" ] || [ -z "$URL" ] || [ -z "$UserId" ] || [ -z "$Functs" ] || [ -z "$Refs" ]; then
+	echo "Usage $0 -l <SiteLabel> -w <FQDN-url> -u <UserId> -f \"[...]\" -r \"[...]\" "
+        echo "  The \"f\" option represents the functionalities and can take several values from :"
+        echo "     - f1 -> Up to 10 pages Audit"
+        echo "     - f2 -> Site Audit "
+        echo "     - f3 -> File upload Audit "
+        echo "  The \"r\" option represents the referential and can take several values from :"
+        echo "     - r1 -> Accessiweb 2.1"
+        echo "     - r2 -> Seo "
 	exit 0
 fi
 
+ref1=NULL;
+ref2=NULL;
+for ref in $Refs;do
+   if [ $ref = "r1" ];
+     then 
+        ref1=1;
+   fi
+   if [ $ref = "r2" ];
+     then 
+        ref2=2;
+   fi
+done
+
+funct1=NULL;
+funct2=NULL;
+funct3=NULL;
+for funct in $Functs;do
+   if [ $funct = "f1" ];
+     then 
+        funct1=1;
+   fi
+   if [ $funct = "f2" ];
+     then 
+        funct2=2;
+   fi
+   if [ $funct = "f3" ];
+     then 
+        funct3=3;
+   fi
+done   
+
+echo $ref1;
+echo $ref2;
+echo $funct1;
+echo $funct2;
+echo $funct3;
+echo "create_contract($UserId, "$SiteLabel", "$URL", $funct1, $funct2, $funct3, $ref1, $ref2);"
 mysql -u $DbUser -p$DbUserPasswd $DbName -e "
-insert into TGSI_CONTRACT (Label, Url, Begin_Date, End_Date, USER_Id_User, PRODUCT_Id_Product) values (\"$SiteLabel\", \"$URL\", date(now()),
-date_add(date(now()), interval 1 year), \"$UserId\", \"$ProductId\");
+call create_contract($UserId, \"$SiteLabel\", \"$URL\", $ref1, $ref2, $funct1, $funct2, $funct3);
 "
