f0c4caa34c503069f67af895131674099d7df8d4
Jérôme Kowalczyk <jk@tanaguru.com>
2012-10-17 13:41:05 +0000

    #515 : list of pages: change link destination (audit // page itself)

--- web-app/tgol-report/src/main/java/org/opens/tgol/report/pagination/PageListWrapper.java
+++ web-app/tgol-report/src/main/java/org/opens/tgol/report/pagination/PageListWrapper.java
@@ -21,12 +21,12 @@
  */
 package org.opens.tgol.report.pagination;
 
-import org.opens.tgol.presentation.data.PageResult;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import org.displaytag.decorator.TableDecorator;
+import org.opens.tgol.presentation.data.PageResult;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.support.RequestContextUtils;
 
@@ -41,10 +41,13 @@ public class PageListWrapper extends TableDecorator {
     private static final String I18N_FILE_KEY = "page-list-page-I18N";
     private static final String PAGE_DETAILED_RESULT_KEY="pageList.pageDetailedResult";
     private static final String NO_DETAILED_RESULT_KEY="pageList.noDetailedResult";
+    private static final String GO_TO_KEY="pageList.goTo";
     private static final String FOR_KEY="pageList.for";
     private static final String NOT_TESTED = "pageList.notTested";
+    private static final String EXTERNAL_LINK_IMG_URL_ATTR_KEY = "externalLinkImgUrl";
     private static final char PERCENT_CHAR='%';
-
+    
+    
     /**
      * Default Constructor
      */
@@ -63,7 +66,7 @@ public class PageListWrapper extends TableDecorator {
             String gradeClass = getGradeClassFromMark(((PageResult)lObject).getRawMark());
             return "<div class=\""+gradeClass+"\"> " + ((PageResult)lObject).getRawMark() + PERCENT_CHAR + " </div>";
         }
-        return getLang(this.getPageContext().getRequest()).getString(NOT_TESTED);
+        return getResourceBundle(this.getPageContext().getRequest()).getString(NOT_TESTED);
     }
     
     /**
@@ -79,28 +82,84 @@ public class PageListWrapper extends TableDecorator {
         }
         return "";
     }
+    
+    /**
+     * Decoration of the displayed raw mark to add the appropriate grade css class.
+     *
+     * @return
+     */
+    public String getUrlWithPageResultLinkAndExternalLink() {
+        Object lObject = this.getCurrentRowObject();
+        ResourceBundle resourceBundle = getResourceBundle(this.getPageContext().getRequest());
+        
+        PageResult pageResult = null;
+        if (lObject instanceof PageResult){
+            pageResult = ((PageResult)lObject);
+        }
+        
+        StringBuilder strb = new StringBuilder();
+        strb.append(buildExternalLinkUrl(pageResult.getUrl(), resourceBundle));
+        strb.append(getUrlLink(pageResult,resourceBundle));
+        return strb.toString();
+    }
 
+    /**
+     * 
+     * @return 
+     */
+    public String getUrlWithExternalLink() {
+        Object lObject = this.getCurrentRowObject();
+        ResourceBundle resourceBundle = getResourceBundle(this.getPageContext().getRequest());
+        
+        PageResult pageResult = null;
+        if (lObject instanceof PageResult){
+            pageResult = ((PageResult)lObject);
+        }
+        
+        StringBuilder strb = new StringBuilder();
+        strb.append(buildExternalLinkUrl(pageResult.getUrl(), resourceBundle));
+        strb.append(pageResult.getUrl());
+        return strb.toString();
+    }
+    
+    private String buildExternalLinkUrl(
+            String url, 
+            ResourceBundle resourceBundle) {
+        StringBuilder strb = new StringBuilder();
+        strb.append("<span class=\"open-external-url-icon\">");
+        strb.append("<a href=\"");
+        strb.append(url);
+        strb.append("\" title=\"");
+        strb.append(getUrlLinkTitle(resourceBundle,url));
+        strb.append("\"><img src=\"");
+        strb.append(this.getPageContext().getAttribute(EXTERNAL_LINK_IMG_URL_ATTR_KEY));
+        strb.append("\" alt=\"");
+        strb.append(getUrlLinkTitle(resourceBundle,url));
+        strb.append("\" /></a></span>");
+        return strb.toString();
+    }
+    
     /**
      * Decoration of the detailed result link to add internationalised link with
      * appropriate internationalised title
      * @return
      */
-    public String getDetailedResultLink() {
-        Object lObject = this.getCurrentRowObject();
-        ResourceBundle resourceBundle = getLang(this.getPageContext().getRequest());
-        int lId = -1;
-        String url = "";
-        String mark = "";
-        if (lObject instanceof PageResult){
-            PageResult pageResult = ((PageResult)lObject);
-            lId = pageResult.getId().intValue();
-            url = pageResult.getUrl();
-            mark = pageResult.getRawMark();
-        }
-        if (Integer.valueOf(mark) > 0 ) {
-            return "<a href=\"audit-result.html?wr=" + lId + "\" title=\""+ getTitle(resourceBundle, url)+"\">"+getLink(resourceBundle)+"</a>";
+    public String getUrlLink(
+            PageResult pageResult,
+            ResourceBundle resourceBundle) {
+
+        int lId = pageResult.getId().intValue();
+        Integer mark = Integer.valueOf(pageResult.getRawMark());
+        if (mark > 0 ) {
+            return "<a href=\"audit-result.html?wr=" + 
+                    lId + 
+                    "\" title=\"" + 
+                    getTitle(resourceBundle, pageResult.getUrl())+
+                    "\">"+
+                    pageResult.getUrl()+
+                    "</a>";
         } else {
-            return getNoDetailedResult(resourceBundle);
+            return pageResult.getUrl() +" ("+getNoDetailedResult(resourceBundle)+")";
         }
     }
 
@@ -110,7 +169,7 @@ public class PageListWrapper extends TableDecorator {
      * @param request
      * @return
      */
-    private ResourceBundle getLang(ServletRequest request) {
+    private ResourceBundle getResourceBundle(ServletRequest request) {
         ResourceBundle resourceBundle = ResourceBundle.getBundle(I18N_FILE_KEY);;
         if (request instanceof HttpServletRequest) {
             LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver((HttpServletRequest) request);
@@ -140,6 +199,17 @@ public class PageListWrapper extends TableDecorator {
      */
     private String getLink(ResourceBundle resourceBundle) {
         return resourceBundle.getString(PAGE_DETAILED_RESULT_KEY);
+    }
+    
+    /**
+     *
+     * @param resourceBundle
+     * @return
+     */
+    private String getUrlLinkTitle(ResourceBundle resourceBundle, String url) {
+        return  resourceBundle.getString(GO_TO_KEY) + 
+                " " +
+                url;
     }
 
     /**
