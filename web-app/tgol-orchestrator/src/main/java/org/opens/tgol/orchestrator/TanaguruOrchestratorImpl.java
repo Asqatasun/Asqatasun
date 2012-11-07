9422b56f9f857b06dfc832b86c719dd37e465790
Jérôme Kowalczyk <jk@tanaguru.com>
2012-11-07 15:18:06 +0000

    tgol-orchestrator : remove bad log message that causes a NullPointer

--- web-app/tgol-orchestrator/src/main/java/org/opens/tgol/orchestrator/TanaguruOrchestratorImpl.java
+++ web-app/tgol-orchestrator/src/main/java/org/opens/tgol/orchestrator/TanaguruOrchestratorImpl.java
@@ -619,7 +619,7 @@ public class TanaguruOrchestratorImpl implements TanaguruOrchestrator {
         }
 
         protected Audit waitForAuditToComplete(Audit audit) {
-            LOGGER.debug("WAIT FOR AUDIT TO COMPLETE:" + audit + "," + audit.getSubject().getURL() + "," +(long) (audit.getDateOfCreation().getTime() / 1000));
+            LOGGER.debug("WAIT FOR AUDIT TO COMPLETE:" + audit + "," + (long) (audit.getDateOfCreation().getTime() / 1000));
             Long token = new Date().getTime();
             this.getAuditExecutionList().put(audit, token);
             // while the audit is not seen as completed or crashed
@@ -828,6 +828,7 @@ public class TanaguruOrchestratorImpl implements TanaguruOrchestrator {
                 int delay) {
             super(auditService, act, parameterSet, locale, delay);
             this.pageUrl = pageUrl;
+            LOGGER.info("auditPage " + pageUrl);
         }
 
         @Override
