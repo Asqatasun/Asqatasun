b4f852a9622368564dda67690c9e7b0bddeab72b
Jérôme Kowalczyk <jk@tanaguru.com>
2013-04-15 16:15:58 +0000

    clean-up tgol-resources project and only package the persistence.xml file in the jar

--- web-app/tgol-resources/src/main/resources/token-master-key-encryptor/generate-encryptor-keys.sh
+++ web-app/tgol-resources/src/main/resources/token-master-key-encryptor/generate-encryptor-keys.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+touch validation.properties
+
 java -classpath lib/esapi.jar:lib/log4j.jar:lib/servlet-api.jar:lib/commons-fileupload.jar -Dorg.owasp.esapi.resources="." org.owasp.esapi.reference.crypto.JavaEncryptor
+
+rm -f validation.properties
