08210f8c912f0c3dbb2644e90d800da2ad347036
Jérôme Kowalczyk <jk@tanaguru.com>
2013-09-23 15:26:20 +0000

    tanaguru-api : creation of cssChecker interface

---
+++ engine/tanaguru-api/src/main/java/org/opens/tanaguru/rules/csschecker/CssChecker.java
@@ -0,0 +1,82 @@
+/*
+ *  Tanaguru - Automated webpage assessment
+ *  Copyright (C) 2008-2013  Open-S Company
+ * 
+ *  This file is part of Tanaguru.
+ * 
+ *  Tanaguru is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ *  Contact us by mail: open-s AT open-s DOT com
+ */
+package org.opens.tanaguru.rules.csschecker;
+
+import com.phloc.css.decl.CascadingStyleSheet;
+import java.util.Collection;
+import org.opens.tanaguru.processor.SSPHandler;
+import org.opens.tanaguru.ruleimplementation.TestSolutionHandler;
+import org.opens.tanaguru.service.NomenclatureLoaderService;
+
+/**
+ * This interface combines selections and checks to be done on css rules.
+ * 
+ * @author jkowalczyk
+ */
+public interface CssChecker {
+    
+    /**
+     * Set the media type list 
+     * 
+     * @param mediaList 
+     */
+    void setMediaList(Collection<String> mediaList);
+    
+    /**
+     * 
+     * @param nomenclatureLoaderService
+     */
+    void setNomenclatureLoaderService(NomenclatureLoaderService nomenclatureLoaderService);
+    
+    /**
+     * 
+     * @return 
+     */
+    NomenclatureLoaderService getNomenclatureLoaderService();
+    
+    /**
+     * 
+     * @return the number of css rules parsed by the checker
+     */
+    int getCssRulesCounter();
+    
+    
+    /**
+     * Perform the check operation. The instance of {@link CascadingStyleSheet} 
+     * received as a parameter is used to retrieve css rules the test is about
+     * and the instance of {@link TestSolutionHandler} received 
+     * as a parameter is used to store the results of tests performed 
+     * during the operation
+     * 
+     * @param sspHandler
+     * @param styleSheetName
+     * @param selectionHandler
+     * @param testSolutionHandler
+     *  
+     */
+    void check (
+            SSPHandler sspHandler, 
+            String styleSheetName,
+            CascadingStyleSheet cascadingStyleSheet, 
+            TestSolutionHandler testSolutionHandler);
+
+}
