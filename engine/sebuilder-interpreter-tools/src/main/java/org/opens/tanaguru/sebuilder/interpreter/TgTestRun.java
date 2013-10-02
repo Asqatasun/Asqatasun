af3e1bf7d8d5e37bed87c1f3e6d2fcb678b8d953
Jérôme Kowalczyk <jk@tanaguru.com>
2013-10-02 15:48:40 +0000

    sebuilder-interpreter-tools : remove unused imports

--- engine/sebuilder-interpreter-tools/src/main/java/org/opens/tanaguru/sebuilder/interpreter/TgTestRun.java
+++ engine/sebuilder-interpreter-tools/src/main/java/org/opens/tanaguru/sebuilder/interpreter/TgTestRun.java
@@ -33,8 +33,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.imageio.ImageIO;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.OutputType;
