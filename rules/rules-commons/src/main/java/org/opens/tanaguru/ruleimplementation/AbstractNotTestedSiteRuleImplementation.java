3c4d8d8e3b7730568e0aa90818a5610e39bd154a
Jérôme Kowalczyk <jk@tanaguru.com>
2013-11-12 16:46:19 +0000

    rules-commons : creation of an abstract not tested site rule implementation that returns not applicable when one page is tested, not_tested instead

---
+++ rules/rules-commons/src/main/java/org/opens/tanaguru/ruleimplementation/AbstractNotTestedSiteRuleImplementation.java
@@ -0,0 +1,82 @@
+/*
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2013  Open-S Company
+ *
+ * This file is part of Tanaguru.
+ *
+ * Tanaguru is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
+ */
+package org.opens.tanaguru.ruleimplementation;
+
+
+import java.util.List;
+import org.opens.tanaguru.entity.audit.DefiniteResult;
+import org.opens.tanaguru.entity.audit.ProcessResult;
+import org.opens.tanaguru.entity.audit.TestSolution;
+import org.opens.tanaguru.entity.subject.Site;
+import org.opens.tanaguru.processor.SSPHandler;
+import org.opens.tanaguru.service.ProcessRemarkService;
+
+/**
+ * <p>
+ * This class should be overridden by concrete {@link RuleImplementation} 
+ * classes which implement tests with site scope.
+ * </p>
+ * A no process rule implementation that returns NOT_APPLICABLE when the 
+ * current audit is a page audit (one page tested) and NOT_TESTED instead. 
+ */
+public abstract class AbstractNotTestedSiteRuleImplementation 
+            extends AbstractSiteRuleImplementation {
+
+    private static final String MOCK_INDEFINITE_RESULT = "mock-result";
+    
+    /**
+     * Default constructor
+     */
+    public AbstractNotTestedSiteRuleImplementation () {
+        super();
+    }
+
+    @Override
+    protected ProcessResult processImpl(SSPHandler sspHandler) {
+        return this.indefiniteResultFactory.create(
+                test, 
+                sspHandler.getPage(), 
+                MOCK_INDEFINITE_RESULT);
+    }
+
+    @Override
+    protected DefiniteResult consolidateSiteImpl(
+            Site group, 
+            List<ProcessResult> groupedGrossResultList, 
+            ProcessRemarkService processRemarkService) {
+        // if only one gross result have been created, that means that only
+        // one page is currently tested. The test is then not_applicable
+        if (groupedGrossResultList.size() == 1) {
+            return this.definiteResultFactory.create(
+                test, 
+                groupedGrossResultList.iterator().next().getSubject(), 
+                TestSolution.NOT_APPLICABLE, 
+                null);
+        };
+        return this.definiteResultFactory.create(
+                test, 
+                groupedGrossResultList.iterator().next().getSubject(), 
+                TestSolution.NOT_TESTED, 
+                null);
+    }
+
+}
