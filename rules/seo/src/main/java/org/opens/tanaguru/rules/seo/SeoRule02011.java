6afbffa36d8d21388b31de908e041f03203ef17d
Jérôme Kowalczyk <jk@tanaguru.com>
2013-11-22 16:00:49 +0000

    seo : 2.1.1 based on rules-commons

--- rules/seo/src/main/java/org/opens/tanaguru/rules/seo/SeoRule02011.java
+++ rules/seo/src/main/java/org/opens/tanaguru/rules/seo/SeoRule02011.java
@@ -26,7 +26,6 @@ import org.opens.tanaguru.rules.elementselector.SimpleElementSelector;
 import org.opens.tanaguru.rules.keystore.AttributeStore;
 import static org.opens.tanaguru.rules.keystore.CssLikeQueryStore.IMG_NOT_IN_LINK_CSS_LIKE_QUERY;
 import static org.opens.tanaguru.rules.keystore.RemarkMessageStore.ALT_MISSING_MSG;
-import static org.opens.tanaguru.rules.keystore.RemarkMessageStore.MORE_THAN_ONE_META_DESC_MSG_CODE;
 
 /**
  * This rule tests if all images have an empty "alt" attribute
@@ -56,38 +55,5 @@ public class SeoRule02011 extends AbstractPageRuleWithSelectorAndCheckerImplemen
                     AttributeStore.SRC_ATTR)
             );
     }
-//extends AbstractPageRuleImplementation {
-//
-//    public static final String CSS_EXPR =
-//            "img:not(a img)";
-//    public static final String ALT_ATTRIBUTE = "alt";
-//    public static final String SRC_EVIDENCE_ELEMENT = "src";
-//
-//    public SeoRule02011() {
-//        super();
-//    }
-//
-//    @Override
-//    protected ProcessResult processImpl(SSPHandler sspHandler) {
-//        sspHandler.beginCssLikeSelection().domCssLikeSelectNodeSet(CSS_EXPR);
-//        TestSolution testSolution = TestSolution.PASSED;
-//        if (sspHandler.getSelectedElements().isEmpty()) {
-//            testSolution = TestSolution.NOT_APPLICABLE;
-//        }
-//        for (Element element : sspHandler.getSelectedElements()) {
-//            if (!element.hasAttr(ALT_ATTRIBUTE)) {
-//                testSolution = TestSolution.FAILED;
-//                sspHandler.getProcessRemarkService().addSourceCodeRemarkOnElement(testSolution, element, "altMissing");
-//            }
-//        }
-//        sspHandler.getProcessRemarkService().addEvidenceElement(SRC_EVIDENCE_ELEMENT);
-//
-//        DefiniteResult result = definiteResultFactory.create(
-//                test,
-//                sspHandler.getSSP().getPage(),
-//                testSolution,
-//                sspHandler.getRemarkList());
-//        result.setElementCounter(sspHandler.getSelectedElementNumber());
-//        return result;
-//    }
+
 }
