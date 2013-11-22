cbe0aadbcbc2a018f9e6fa2f7ca240e76119e976
Jérôme Kowalczyk <jk@tanaguru.com>
2013-11-22 16:37:12 +0000

    seo : 2.1.2 based on rules-commons

--- rules/seo/src/main/java/org/opens/tanaguru/rules/seo/SeoRule02012.java
+++ rules/seo/src/main/java/org/opens/tanaguru/rules/seo/SeoRule02012.java
@@ -19,76 +19,97 @@
  */
 package org.opens.tanaguru.rules.seo;
 
-import java.util.Iterator;
-import org.apache.commons.lang3.StringUtils;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.opens.tanaguru.entity.audit.DefiniteResult;
-import org.opens.tanaguru.entity.audit.ProcessResult;
 import org.opens.tanaguru.entity.audit.TestSolution;
-import org.opens.tanaguru.processor.SSPHandler;
-import org.opens.tanaguru.ruleimplementation.AbstractPageRuleImplementation;
+import org.opens.tanaguru.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.opens.tanaguru.rules.elementchecker.attribute.AttributePresenceChecker;
+import org.opens.tanaguru.rules.elementselector.AreaElementSelector;
+import static org.opens.tanaguru.rules.keystore.AttributeStore.ALT_ATTR;
+import static org.opens.tanaguru.rules.keystore.AttributeStore.HREF_ATTR;
+import static org.opens.tanaguru.rules.keystore.RemarkMessageStore.ALT_MISSING_MSG;
 
 /**
  * This rule tests if an area tag defined in a map tag, associated with the usemap
  * attribute of an img tag, has an alt attribute
  * @author jkowalczyk
  */
-public class SeoRule02012 extends AbstractPageRuleImplementation {
+public class SeoRule02012 extends AbstractPageRuleWithSelectorAndCheckerImplementation {
 
-    public static final String HREF_EVIDENCE_ELEMENT = "href";
-    public static final String CSS_EXPR = 
-            "map[name] area";
-    public static final String CSS2_EXPR = 
-            "img[usemap]";
-
-    public static final String ALT_ATTRIBUTE = "alt";
-
-    public SeoRule02012(){
-        super();
+    /**
+     * Default constructor
+     */
+    public SeoRule02012() {
+        super(
+                new AreaElementSelector(), 
+                new AttributePresenceChecker(
+                        ALT_ATTR, 
+                        // passed when attribute is found
+                        TestSolution.PASSED, 
+                        // failed when attribute is not found
+                        TestSolution.FAILED, 
+                        // no message created when attribute is found
+                        null, 
+                        // message associated with element when attribute is not found
+                        ALT_MISSING_MSG,
+                        // evidence elements,
+                        HREF_ATTR)
+            );
     }
 
-    @Override
-    protected ProcessResult processImpl(SSPHandler sspHandler) {
-        Elements imageMapElement = sspHandler.beginCssLikeSelection().domCssLikeSelectNodeSet(CSS2_EXPR).getSelectedElements();
-        Elements areaElement = sspHandler.domCssLikeSelectNodeSet(CSS_EXPR).getSelectedElements();
-
-        Iterator<Element> iter = areaElement.iterator();
-        while (iter.hasNext()) {
-            Element element = iter.next();
-            String mapParentNameValue = element.parent().attr("name");
-            boolean hasRelativeImage = false;
-            for (Element imgElement : imageMapElement) {
-                if (StringUtils.equals(imgElement.attr("usemap"),"#"+mapParentNameValue) || 
-                        StringUtils.equals(imgElement.attr("usemap"),mapParentNameValue)) {
-                    hasRelativeImage = true;
-                    break;
-                } 
-            }
-            if (!hasRelativeImage) {
-                iter.remove();
-            }
-        }
-
-        TestSolution checkResult = TestSolution.PASSED;
-        if (areaElement.isEmpty()) {
-            checkResult = TestSolution.NOT_APPLICABLE;
-        }
-        for(Element el : areaElement) {
-            if (!el.hasAttr("alt")) {
-                checkResult = TestSolution.FAILED;
-                sspHandler.getProcessRemarkService().addSourceCodeRemarkOnElement(checkResult, el, "altMissing");
-            }
-        }
-
-        DefiniteResult result = definiteResultFactory.create(
-                test,
-                sspHandler.getSSP().getPage(),
-                checkResult,
-                sspHandler.getRemarkList());
-
-        result.setElementCounter(areaElement.size());
-        
-        return result;
-    }
+//extends AbstractPageRuleImplementation {
+//
+//    public static final String HREF_EVIDENCE_ELEMENT = "href";
+//    public static final String CSS_EXPR = 
+//            "map[name] area";
+//    public static final String CSS2_EXPR = 
+//            "img[usemap]";
+//
+//    public static final String ALT_ATTRIBUTE = "alt";
+//
+//    public SeoRule02012(){
+//        super();
+//    }
+//
+//    @Override
+//    protected ProcessResult processImpl(SSPHandler sspHandler) {
+//        Elements imageMapElement = sspHandler.beginCssLikeSelection().domCssLikeSelectNodeSet(CSS2_EXPR).getSelectedElements();
+//        Elements areaElement = sspHandler.domCssLikeSelectNodeSet(CSS_EXPR).getSelectedElements();
+//
+//        Iterator<Element> iter = areaElement.iterator();
+//        while (iter.hasNext()) {
+//            Element element = iter.next();
+//            String mapParentNameValue = element.parent().attr("name");
+//            boolean hasRelativeImage = false;
+//            for (Element imgElement : imageMapElement) {
+//                if (StringUtils.equals(imgElement.attr("usemap"),"#"+mapParentNameValue) || 
+//                        StringUtils.equals(imgElement.attr("usemap"),mapParentNameValue)) {
+//                    hasRelativeImage = true;
+//                    break;
+//                } 
+//            }
+//            if (!hasRelativeImage) {
+//                iter.remove();
+//            }
+//        }
+//
+//        TestSolution checkResult = TestSolution.PASSED;
+//        if (areaElement.isEmpty()) {
+//            checkResult = TestSolution.NOT_APPLICABLE;
+//        }
+//        for(Element el : areaElement) {
+//            if (!el.hasAttr("alt")) {
+//                checkResult = TestSolution.FAILED;
+//                sspHandler.getProcessRemarkService().addSourceCodeRemarkOnElement(checkResult, el, "altMissing");
+//            }
+//        }
+//
+//        DefiniteResult result = definiteResultFactory.create(
+//                test,
+//                sspHandler.getSSP().getPage(),
+//                checkResult,
+//                sspHandler.getRemarkList());
+//
+//        result.setElementCounter(areaElement.size());
+//        
+//        return result;
+//    }
 }
