5600dcd7a10da76c9a8059e1d0751f4181d52224
Jérôme Kowalczyk <jk@tanaguru.com>
2013-12-11 09:01:51 +0000

    seo : add header of test classes

--- rules/seo/src/test/java/org/opens/tanaguru/rules/seo/SeoRule06011Test.java
+++ rules/seo/src/test/java/org/opens/tanaguru/rules/seo/SeoRule06011Test.java
@@ -1,14 +1,21 @@
 /*
- * @(#)SeoRule06011Test.java
+ * Tanaguru - Automated webpage assessment
+ * Copyright (C) 2008-2013  Open-S Company
  *
- * Copyright  2010 SAS OPEN-S. All rights reserved.
- * OPEN-S PROPRIETARY/CONFIDENTIAL.  Use is subject to license terms.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- * This file is  protected by the  intellectual  property rights
- * in  France  and  other  countries, any  applicable  copyrights  or
- * patent rights, and international treaty provisions. No part may be
- * reproduced  in  any  form  by  any  mean  without   prior  written
- * authorization of OPEN-S.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact us by mail: open-s AT open-s DOT com
  */
 package org.opens.tanaguru.rules.seo;
 
