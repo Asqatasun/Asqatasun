5ffdb9d253fad0ccec5cc60c70dbc602de553a88
medsob <medsob@users.noreply.github.com>
2014-04-08 17:52:24 +0200

    MSO [Web Service - Tache #510] Couche DAO avec TU

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/entity/impl/WsUserImpl.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/entity/impl/WsUserImpl.java
@@ -5,25 +5,57 @@ import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 import com.oceaneconsulting.tanaguru.entity.WsUser;
 
+/**
+ * Classe de l'utilsateur
+ * @author msobahi
+ *
+ */
 @Entity
 @Table(name = "WS_USER")
 @XmlRootElement
 public class WsUserImpl implements WsUser, Serializable {
 
+	/**
+	 * Identifiant
+	 */
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "ID_USER")
 	private Long id;
 	
+	/**
+	 * Email de l'utilisateur
+	 */
+	@Column(name ="EMAIL")
+	private String email;
+	
+	/**
+	 * Le mot de passe
+	 */
+	@Column(name="PASSWORD")
+	private String password;
+	
+	/**
+	 * Nom de l'utilisateur
+	 */
 	@Column(name = "NAME")
 	private String name;
 	
+	/**
+	 * Prénom de l'utilisateur
+	 */
 	@Column (name = "FIRST_NAME")
 	private String firstName;
 	
+	/**
+	 * Flag d'activité (0 ou 1)
+	 */
 	@Column (name ="ACTIVE")
 	private Boolean active;
 	
+	/**
+	 * Le role de l'utilisateur
+	 */
 	@ManyToOne 
 	@JoinColumn(name="ID_ROLE", nullable = false)
 	private WsRoleImpl role;
@@ -38,6 +70,26 @@ public class WsUserImpl implements WsUser, Serializable {
 		this.id = id;
 	}
 	
+	@Override
+	public String getEmail() {
+		return email;
+	}
+
+	@Override
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	@Override
 	public String getName(){
 		return this.name;
