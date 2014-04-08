6ab3014be953328680cffadc7be0c0849500369d
medsob <medsob@users.noreply.github.com>
2014-04-08 17:48:46 +0200

    MSO [Web Service - Tache #510] Couche DAO avec TU

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/dao/WsRoleDao.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/dao/WsRoleDao.java
@@ -3,6 +3,11 @@ package com.oceaneconsulting.tanaguru.dao;
 import org.opens.tanaguru.sdk.entity.dao.GenericDAO;
 import com.oceaneconsulting.tanaguru.entity.WsRole;
 
+/**
+ * Interface de la DAO des rôles
+ * @author msobahi
+ *
+ */
 public interface WsRoleDao extends GenericDAO<WsRole, Long> {
 
 }
--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/dao/WsUserDao.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/dao/WsUserDao.java
@@ -3,6 +3,11 @@ package com.oceaneconsulting.tanaguru.dao;
 import org.opens.tanaguru.sdk.entity.dao.GenericDAO;
 import com.oceaneconsulting.tanaguru.entity.WsUser;
 
+/**
+ * Interface de la DAO des utilisateurs
+ * @author msobahi
+ *
+ */
 public interface WsUserDao extends GenericDAO<WsUser, Long> {
 
 }
