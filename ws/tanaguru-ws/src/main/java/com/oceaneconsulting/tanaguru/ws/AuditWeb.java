417c500be44e62006c925f49fdab81c9c78308c8
sofien-hamdi <sofien.hamdi@gmail.com>
2014-04-10 17:46:05 +0200

    jax-rs v2.0 update, unfinished unit tests

---
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/ws/AuditWeb.java
@@ -0,0 +1,25 @@
+package com.oceaneconsulting.tanaguru.ws;
+
+
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.core.Response;
+
+
+
+/**
+ * Webservice interface definition.
+ * 
+ * This interface defines all exposed webservice's operations. 
+ *
+ */
+public interface AuditWeb {
+	
+
+    public void auditPage(final String pageURL, final String level /*, final HttpServletRequest request*/, final AsyncResponse response);
+	
+	//As a first step, json file is defined as a simple string. This may change in future versions.
+	public Response auditScenario( String scenarioName, String scenarioText , String level);
+	
+	public Response auditSite(String siteURL, String level);
+	
+}
