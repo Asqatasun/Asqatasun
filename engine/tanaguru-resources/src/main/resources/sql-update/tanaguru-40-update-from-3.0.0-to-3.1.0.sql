903cb31370c22e2b356e24130c5d7dd2e5ec66d4
tellouze <ellouze.toufik@gmail.com>
2014-04-10 17:56:23 +0200

    new column in table audit for manual audit creation

--- engine/tanaguru-resources/src/main/resources/sql-update/tanaguru-40-update-from-3.0.0-to-3.1.0.sql
+++ engine/tanaguru-resources/src/main/resources/sql-update/tanaguru-40-update-from-3.0.0-to-3.1.0.sql
@@ -15,3 +15,10 @@ COMMENT 'Manual audit overridden definite value' AFTER `Id_Test`;
 ALTER TABLE `process_result` 
 ADD COLUMN `Manual_Audit_Comment` VARCHAR(255) NULL DEFAULT NULL 
 COMMENT 'Manual audit user comment' AFTER `Manual_Definite_Value`;
+
+-- -----------------------------------------------------
+-- Column `Manual_Audit_Dt_Creation` to store the manual 
+-- audit launch dates
+-- -----------------------------------------------------
+ALTER TABLE `AUDIT` 
+ADD COLUMN `Manual_Audit_Dt_Creation` DATETIME NULL DEFAULT NULL AFTER `Dt_Creation`;
