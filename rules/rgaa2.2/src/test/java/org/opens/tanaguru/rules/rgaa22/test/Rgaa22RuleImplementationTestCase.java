8b0187c40c6fffd68d7370ebc39b91564b65d872
Jerome Kowalczyk <jk@tanaguru.com>
2014-06-30 14:52:51 +0200

    refactor rule packaging to be handled by one project

--- rules/rgaa2.2/src/test/java/org/opens/tanaguru/rules/rgaa22/test/Rgaa22RuleImplementationTestCase.java
+++ rules/rgaa2.2/src/test/java/org/opens/tanaguru/rules/rgaa22/test/Rgaa22RuleImplementationTestCase.java
@@ -27,8 +27,8 @@ import org.opens.tanaguru.rules.test.AbstractRuleImplementationTestCase;
  */
 public abstract class Rgaa22RuleImplementationTestCase extends AbstractRuleImplementationTestCase {
 
-    private static final String TESTCASE_FILE_PATH = "../rgaa2.2-testcases/src/main/resources/testcases/";
-    private static final String INPUT_FILE_DATA_NAME = "../rgaa2.2-testcases/src/main/resources/dataSets/nomenclatureFlatXmlDataSet.xml";
+    private static final String TESTCASE_FILE_PATH = "src/test/resources/testcases/";
+    private static final String INPUT_FILE_DATA_NAME = "src/test/resources/dataSets/nomenclatureFlatXmlDataSet.xml";
 
     public Rgaa22RuleImplementationTestCase(String testName) {
         super(testName, INPUT_FILE_DATA_NAME, TESTCASE_FILE_PATH);
