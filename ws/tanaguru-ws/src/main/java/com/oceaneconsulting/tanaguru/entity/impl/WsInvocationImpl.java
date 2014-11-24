46638f05deaa974c1be523d4447e04f63b5113e5
Jérôme Kowalczyk <jk@tanaguru.com>
2014-11-24 16:44:11 +0100

    Class formatting (apply ident policy)

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/entity/impl/WsInvocationImpl.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/entity/impl/WsInvocationImpl.java
@@ -15,150 +15,156 @@ import java.util.Date;
 @Table(name = "WS_INVOCATION")
 @XmlRootElement
 public class WsInvocationImpl implements WsInvocation, Serializable {
-	  
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 7482159329304767809L;
-	
-	/**
-	 * Identifiant  
-	 */
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 7482159329304767809L;
+
+    /**
+     * Identifiant
+     */
     @Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "ID_INVOCATION")
-	private Long id;
-	/**
-	 * Nom du host
-	 */
-    @Column(name = "HOST_NAME") 
-	private String hostName;
-	
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID_INVOCATION")
+    private Long id;
+    /**
+     * Nom du host
+     */
+    @Column(name = "HOST_NAME")
+    private String hostName;
+
     /**
      * L'adresse IP du host
      */
     @Column(name = "HOST_IP")
-	private String hostIp;
-	
+    private String hostIp;
+
     /**
      * Date d'invocation
      */
     @Column(name = "DT_INVOCATION")
-	private Date dateInvocation;
-	
+    @Temporal(javax.persistence.TemporalType.TIMESTAMP)
+    private Date dateInvocation;
+
     /**
      * Le type d'audit
      */
     @Column(name = "AUDIT_TYPE")
-	private Integer auditType;
-	
+    private Integer auditType;
+
     /**
      * L'utilsateur qui a lanc l'audit
      */
     @ManyToOne
-    @JoinColumn(name = "ID_USER", nullable = false)
-	private WsUserImpl user;
+    @JoinColumn(name = "ID_USER")
+    private WsUserImpl user;
 
     /**
      * Audit identifier
      */
     @Column(name = "ID_AUDIT")
-	private Long auditId;
-	
+    private Long auditId;
+
     /**
      * Audit category
      */
     @Column(name = "CATEGORY")
-	private String category;
-	
+    private String category;
+
     /**
      * Audit country
      */
     @Column(name = "COUNTRY")
-	private String country;
-    
-    
+    private String country;
+
     @Override
-    public Long getId(){
-    	return this.id;
-    }
-    
-    @Override
-	public void setId(Long id){
-    	this.id = id;
-    }
-    
-    @Override
-	public String getHostName(){
-    	return this.hostName;
-    }
-    
-    @Override
-	public void setHostName(String hostName){
-    	this.hostName = hostName;
-    }
-    
-    @Override
-	public String getHostIp(){
-    	return this.hostIp;
-    }
-    
-    @Override
-	public void setHostIp(String hostIp){
-    	this.hostIp = hostIp;
-    }
-    
-    @Override
-	public Date getDateInvocation(){
-    	return dateInvocation;
-    }
-    
-    @Override
-	public void setDateInvocation(Date dateInvocation){
-    	this.dateInvocation = dateInvocation;
-    }
-    
-    @Override
-	public Integer getAuditType(){
-    	return this.auditType;
-    }
-    
-    @Override
-	public void setAuditType(Integer auditType){
-    	this.auditType = auditType;
-    }
-    
-    @Override
-	public WsUserImpl getUser(){
-    	return user;
-    }
-    
-    @Override
-	public void setUser(WsUserImpl user){
-    	this.user = user;
+    public Long getId() {
+        return this.id;
     }
 
-	public Long getAuditId() {
-		return auditId;
-	}
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public void setAuditId(Long auditId) {
-		this.auditId = auditId;
-	}
+    @Override
+    public String getHostName() {
+        return this.hostName;
+    }
 
-	public String getCategory() {
-		return category;
-	}
+    @Override
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
 
-	public void setCategory(String category) {
-		this.category = category;
-	}
+    @Override
+    public String getHostIp() {
+        return this.hostIp;
+    }
 
-	public String getCountry() {
-		return country;
-	}
+    @Override
+    public void setHostIp(String hostIp) {
+        this.hostIp = hostIp;
+    }
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+    @Override
+    public Date getDateInvocation() {
+        return dateInvocation;
+    }
+
+    @Override
+    public void setDateInvocation(Date dateInvocation) {
+        this.dateInvocation = dateInvocation;
+    }
+
+    @Override
+    public Integer getAuditType() {
+        return this.auditType;
+    }
+
+    @Override
+    public void setAuditType(Integer auditType) {
+        this.auditType = auditType;
+    }
+
+    @Override
+    public WsUserImpl getUser() {
+        return user;
+    }
+
+    @Override
+    public void setUser(WsUserImpl user) {
+        this.user = user;
+    }
+
+    @Override
+    public Long getAuditId() {
+        return auditId;
+    }
+
+    @Override
+    public void setAuditId(Long auditId) {
+        this.auditId = auditId;
+    }
+
+    @Override
+    public String getCategory() {
+        return category;
+    }
+
+    @Override
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    @Override
+    public String getCountry() {
+        return country;
+    }
+
+    @Override
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/enumerations/AuditLevel.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/enumerations/AuditLevel.java
@@ -2,55 +2,48 @@ package com.oceaneconsulting.tanaguru.enumerations;
 
 /**
  * Type de niveau d'audit
- * 
+ *
  * @author diamamp
- * 
+ *
  */
 public enum AuditLevel {
-	AW22_OR("AW22;LEVEL_3"), 
-	AW22_AR("AW22;LEVEL_2"),
-	AW22_BZ("AW22;LEVEL_1");
-    
+
+    AW22_OR("Aw22;LEVEL_3"),
+    AW22_AR("Aw22;LEVEL_2"),
+    AW22_BZ("Aw22;LEVEL_1");
+
     private String value;
 
-    private AuditLevel()
-    {
+    private AuditLevel() {
         // TODO Auto-generated constructor stub
     }
 
-    private AuditLevel( String value )
-    {
+    private AuditLevel(String value) {
         this.value = value;
     }
 
-    public AuditLevel getStatusFromValue( String statusName )
-    {
-        for ( AuditLevel statusType : AuditLevel.values() )
-        {
-            if ( statusType.getValue().equals( statusName ) )
-            {
+    public AuditLevel getStatusFromValue(String statusName) {
+        for (AuditLevel statusType : AuditLevel.values()) {
+            if (statusType.getValue().equals(statusName)) {
                 return statusType;
             }
         }
-        for ( AuditLevel statusType : AuditLevel.values() )
-        {
-            if ( statusType.name().equals( statusName ) )
-            {
+        for (AuditLevel statusType : AuditLevel.values()) {
+            if (statusType.name().equals(statusName)) {
                 return statusType;
             }
         }
         return null;
     }
 
-    public String getValue()
-    {
+    public String getValue() {
         return value;
     }
 
-    public void setValue( String value )
-    {
+    public void setValue(String value) {
         this.value = value;
     }
+
     public static boolean contains(String value) {
 
         for (AuditLevel c : AuditLevel.values()) {
@@ -61,16 +54,13 @@ public enum AuditLevel {
 
         return false;
     }
-    public boolean equals( String value )
-    {
-        AuditLevel statusType = getStatusFromValue( value );
-        if ( statusType != null )
-        {
-            return this.equals( statusType );
+
+    public boolean equals(String value) {
+        AuditLevel statusType = getStatusFromValue(value);
+        if (statusType != null) {
+            return this.equals(statusType);
         }
         return false;
     }
 
-
-    
 }
