052b2ef01d9c1e844170e5e58cc711a6acf4e38c
Jérôme Kowalczyk <jk@tanaguru.com>
2014-11-24 16:55:02 +0100

    Expose audit and webresources entities in API

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/decorator/impl/WebResourceDataServiceDecoratorImpl.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/decorator/impl/WebResourceDataServiceDecoratorImpl.java
@@ -1,6 +1,5 @@
 package com.oceaneconsulting.tanaguru.decorator.impl;
 
-
 import java.util.List;
 
 import org.opens.tanaguru.entity.service.subject.WebResourceDataService;
@@ -9,79 +8,86 @@ import org.opens.tanaguru.entity.subject.Site;
 import org.opens.tanaguru.entity.subject.WebResource;
 import org.opens.tanaguru.sdk.entity.service.AbstractGenericDataService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
 
 import com.oceaneconsulting.tanaguru.dao.StatisticsDAO;
 import com.oceaneconsulting.tanaguru.decorator.WebResourceDataServiceDecorator;
 import com.oceaneconsulting.tanaguru.ws.types.AuditResult;
 import com.oceaneconsulting.tanaguru.ws.types.GlobalStatsOrder;
+import org.opens.tanaguru.entity.audit.EvidenceElement;
+import org.opens.tanaguru.entity.audit.ProcessRemark;
+import org.opens.tanaguru.entity.audit.ProcessResult;
+import org.springframework.stereotype.Service;
 
 /**
- * This class wrap {@link org.opens.tanaguru.entity.service.subject.WebResourceDataService} class. 
- * It enlarge default webresource data service by adding specific statistics.
+ * This class wrap
+ * {@link org.opens.tanaguru.entity.service.subject.WebResourceDataService}
+ * class. It enlarge default webresource data service by adding specific
+ * statistics.
  *
  * @author shamdi at oceaneconsulting dot com
  *
  */
 @Service("webResourceDataServiceDecorator")
-public class WebResourceDataServiceDecoratorImpl extends AbstractGenericDataService<WebResource, Long>  implements WebResourceDataServiceDecorator {
-		
-		@Autowired
-		@Qualifier("webResourceDataService")
-	    private WebResourceDataService webResourceDataService;
-	    
-		@Autowired
-		private StatisticsDAO statisticsDAO;
+public class WebResourceDataServiceDecoratorImpl extends AbstractGenericDataService<WebResource, Long> 
+                implements WebResourceDataServiceDecorator {
 
+    @Autowired
+    private WebResourceDataService webResourceDataService;
 
-	    public AuditResult findWeightedMarkAndStatusByAuditId(Long idAudit) {
-	    	return statisticsDAO.findWeightedMarkAndStatusByAuditId(idAudit);
-	    }
-		
-	    public List<AuditResult> findWeightedMarkAndStatus(GlobalStatsOrder globalStatsOrder) {
-	    	return statisticsDAO.findWeightedMarkAndStatus(globalStatsOrder);
-		}
-		
-	    //TODO get statistics for audit site 
+    @Autowired
+    private StatisticsDAO statisticsDAO;
 
-		@Override
-		public Page createPage(String arg0) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public AuditResult findWeightedMarkAndStatusByAuditId(Long idAudit) {
+        return statisticsDAO.findWeightedMarkAndStatusByAuditId(idAudit);
+    }
 
-		@Override
-		public Site createSite(String arg0) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public List<AuditResult> findWeightedMarkAndStatus(GlobalStatsOrder globalStatsOrder) {
+        return statisticsDAO.findWeightedMarkAndStatus(globalStatsOrder);
+    }
 
-		@Override
-		public WebResource getByUrl(String arg0) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public WebResource getWebResourceFullDeep(Long id) {
+        WebResource wr = this.read(id);
+        for (ProcessResult pr : wr.getProcessResultList()) {
+            for (ProcessRemark prk : pr.getRemarkSet()) {
+                for (EvidenceElement ee : prk.getElementList()) {}
+            }
+        }
+        return wr;
+    }
+    
+    @Override
+    public Page createPage(String arg0) {
+        return webResourceDataService.createPage(arg0);
+    }
 
-		@Override
-		public WebResource getByUrlAndParentWebResource(String arg0,
-				WebResource arg1) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public Site createSite(String arg0) {
+        return webResourceDataService.createSite(arg0);
+    }
 
-		@Override
-		public Long getNumberOfChildWebResource(WebResource arg0) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public WebResource getByUrl(String arg0) {
+        return webResourceDataService.getByUrl(arg0);
+    }
 
-		@Override
-		public List<WebResource> getWebResourceFromItsParent(WebResource arg0,
-				int arg1, int arg2) {
-			//Not implemented
-			return null;
-		}
+    @Override
+    public WebResource getByUrlAndParentWebResource(String arg0,
+            WebResource arg1) {
+        return webResourceDataService.getByUrlAndParentWebResource(arg0, arg1);
+    }
 
+    @Override
+    public Long getNumberOfChildWebResource(WebResource arg0) {
+        return webResourceDataService.getNumberOfChildWebResource(arg0);
+    }
 
-	}
+    @Override
+    public List<WebResource> getWebResourceFromItsParent(WebResource arg0,
+            int arg1, int arg2) {
+        return webResourceDataService.getWebResourceFromItsParent(arg0, arg1, arg2);
+    }
+
+}
