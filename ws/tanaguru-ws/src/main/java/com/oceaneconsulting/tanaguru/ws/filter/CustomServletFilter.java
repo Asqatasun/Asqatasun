571da4b8234a0fdb5956a741c86b1f899a05d45a
Jérôme Kowalczyk <jk@tanaguru.com>
2014-11-24 16:54:22 +0100

    Class formatting (apply ident policy)

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/ws/filter/CustomServletFilter.java
+++ ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/ws/filter/CustomServletFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.log4j.Logger;
 import org.springframework.web.context.support.WebApplicationContextUtils;
 
@@ -23,72 +23,75 @@ import com.oceaneconsulting.tanaguru.service.WsInvocationService;
 
 /**
  * Custom filter implementing specific limitation rules.
- * 
+ *
  * @author shamdi at oceaneconsulting dot com
  *
  */
-@WebFilter(urlPatterns = {"/*" }, asyncSupported = true)
+@WebFilter(urlPatterns = {"/*"}, asyncSupported = true)
 public class CustomServletFilter implements Filter {
 
-	private static final Logger LOGGER = Logger.getLogger(CustomServletFilter.class);
-	
-	WsInvocationService bean = null;
-	
-	@Override
-	public void init(FilterConfig config) throws ServletException {
-		if(bean == null){
-			LOGGER.debug("Getting WS invocation service bean.");
-			bean = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext()).getBean(WsInvocationService.class);
-		}
+    private static final Logger LOGGER = Logger.getLogger(CustomServletFilter.class);
 
-	}
+    WsInvocationService bean = null;
 
-	@Override
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        if (bean == null) {
+            LOGGER.debug("Getting WS invocation service bean.");
+            bean = WebApplicationContextUtils.getRequiredWebApplicationContext(config.getServletContext()).getBean(WsInvocationService.class);
+        }
 
-		try {
-			if("GET".equals(((HttpServletRequest)request).getMethod())){ //audit page
+    }
 
-				boolean access = Boolean.FALSE;
-				LOGGER.info("Access control launched...");
-				LOGGER.debug("Audit page access control for host name ="+request.getRemoteHost()+", ip="+request.getRemoteAddr());
-				
-				//save user information
-				WsInvocation invocation = new WsInvocationImpl();
-				invocation.setDateInvocation(new Date());
-				invocation.setHostIp(request.getRemoteAddr());
-				invocation.setHostName(request.getRemoteHost());
-				invocation.setAuditType(0);
-				invocation.setUser(null);//unnecessary information
-				bean.create(invocation);
-				
-				//verify user access limitation
-				access = bean.checkLimitationOverflow(null, request.getRemoteHost(), request.getRemoteAddr(), null);
-				
-				LOGGER.debug("Verification complete, allowed =" + access);
-				
-				if(!access){
-					 ((HttpServletResponse)response).sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "You reached the maximum number of audits allowed for today...");
-				} else {
-					LOGGER.debug("Request filter process...");
-					chain.doFilter(request, response);
-					LOGGER.debug("Request filter OK.");
-				}
-			} else if("POST".equals(((HttpServletRequest)request).getMethod())){ //site audit 
-				LOGGER.debug("Request filter process...");
-				chain.doFilter(request, response);
-				LOGGER.debug("Request filter OK.");
-			} else {//other methods are considered as bad request
-				((HttpServletResponse)response).sendError(HttpServletResponse.SC_BAD_REQUEST);
-			}
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-		} catch (Exception e) {
-			LOGGER.debug(ExceptionUtils.getFullStackTrace(e));
-			((HttpServletResponse)response).sendError(HttpServletResponse.SC_BAD_REQUEST);
-		}
-	}
+        try {
+            if (null != ((HttpServletRequest) request).getMethod()) switch (((HttpServletRequest) request).getMethod()) {
+                case "GET":
+                    //audit page
+                    
+                    LOGGER.info("Access control launched...");
+                    LOGGER.debug("Audit page access control for host name =" + request.getRemoteHost() + ", ip=" + request.getRemoteAddr());
+                    //save user information
+                    WsInvocation invocation = new WsInvocationImpl();
+                    invocation.setDateInvocation(new Date());
+                    invocation.setHostIp(request.getRemoteAddr());
+                    invocation.setHostName(request.getRemoteHost());
+                    invocation.setAuditType(0);
+                    invocation.setUser(null);//unnecessary information
+                    bean.create(invocation);
+                    //verify user access limitation
+                    boolean access = bean.checkLimitationOverflow(null, request.getRemoteHost(), request.getRemoteAddr(), null);
+                    LOGGER.debug("Verification complete, allowed =" + access);
+                    if (!access) {
+                        ((HttpServletResponse) response).sendError(
+                                HttpServletResponse.SC_METHOD_NOT_ALLOWED, 
+                                "You reached the maximum number of audits allowed for today...");
+                    } else {
+                        LOGGER.debug("Request filter process... "  +chain.getClass() );
+                        chain.doFilter(request, response);
+                        LOGGER.debug("Request filter OK.");
+                    }   break;
+                case "POST":
+                    //site audit
+                    LOGGER.debug("Request filter process...");
+                    chain.doFilter(request, response);
+                    LOGGER.debug("Request filter OK.");
+                    break;
+                default:
+                    //other methods are considered as bad request
+                    ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST);
+                    break;
+            }
 
-	@Override
-	public void destroy() {
-	}
+        } catch (IOException | ServletException e) {
+            LOGGER.debug(ExceptionUtils.getStackTrace(e));
+            ((HttpServletResponse) response).sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+    @Override
+    public void destroy() {
+    }
 }
