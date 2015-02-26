f0112cf788ad3fef4937d1b633fc034945bb59a2
Jérôme Kowalczyk <jk@tanaguru.com>
2015-02-26 23:26:38 +0100

    correction of script execution that set all results to NA (even the non-na) when click on

--- web-app/tgol-web-app/src/main/webapp/Js/result-page/apply-auto-results.js
+++ web-app/tgol-web-app/src/main/webapp/Js/result-page/apply-auto-results.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 
     $('#apply-all-auto-passed').click(function () {
         $('.audit-result-container').each(function () {
-            var passed = $(this).find('.test-result img[src*="passed"]'); 
+            var passed = $(this).find('.test-result img[src*="ico-passed"]'); 
             if (passed.length > 0) {
                 $(this).find('.audit-result-manual input[value*="passed"]').click();
 //                $(this).find('.audit-result-manual input[value*="passed"]').attr('checked', 'checked');
@@ -14,7 +14,7 @@ $(document).ready(function() {
     });
     $('#apply-all-auto-failed').click(function () {
         $('.audit-result-container').each(function () {
-            var failed = $(this).find('.test-result img[src*="failed"]'); 
+            var failed = $(this).find('.test-result img[src*="ico-failed"]'); 
             if (failed.length > 0) {
                 $(this).find('.audit-result-manual input[value*="failed"]').click();
 //                $(this).find('.audit-result-manual input[value*="failed"]').attr('checked', 'checked');
@@ -23,7 +23,7 @@ $(document).ready(function() {
     });
     $('#apply-all-auto-na').click(function () {
         $('.audit-result-container').each(function () {
-            var na = $(this).find('.test-result img[src*="na"]'); 
+            var na = $(this).find('.test-result img[src*="ico-na"]'); 
             if (na.length > 0) {
                 $(this).find('.audit-result-manual input[value*="na"]').click();
 //                $(this).find('.audit-result-manual input[value*="na"]').attr('checked', 'checked');
