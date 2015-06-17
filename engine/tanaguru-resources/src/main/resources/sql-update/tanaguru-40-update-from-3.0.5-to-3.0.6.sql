1b84ae104666c2ca8720d4e1d94758ca9b1a78c4
Jerome Kowalczyk <jk@tanaguru.com>
2015-06-17 22:26:41 +0200

    update sql insertion script to deal with dynamic id generation and usage

--- engine/tanaguru-resources/src/main/resources/sql-update/tanaguru-40-update-from-3.0.5-to-3.0.6.sql
+++ engine/tanaguru-resources/src/main/resources/sql-update/tanaguru-40-update-from-3.0.5-to-3.0.6.sql
@@ -3,10 +3,9 @@ SET foreign_key_checks=0;
 DELETE FROM PARAMETER WHERE Id_Parameter_Element in (select Id_Parameter_Element FROM PARAMETER_ELEMENT WHERE Cd_Parameter_Element like "%PROXY%");
 DELETE FROM PARAMETER_ELEMENT WHERE Cd_Parameter_Element like "%PROXY%";
 
-INSERT IGNORE INTO `PARAMETER_ELEMENT` (`Cd_Parameter_Element`, `Long_Label`, `Short_Label`) VALUES
-('COMPLEX_TABLE_MARKER', 'Complex table marker', 'Correspond to the attribute \"id\", \"class\" or \"role\" of the complex tables');
+INSERT IGNORE INTO `PARAMETER_ELEMENT` (`Cd_Parameter_Element`,`Id_Parameter_Family`, `Long_Label`, `Short_Label`) VALUES
+('COMPLEX_TABLE_MARKER', (SELECT `Id_Parameter_Family` FROM `PARAMETER_FAMILY` WHERE `Cd_Parameter_Family` LIKE 'RULES'), 'Complex table marker', 'Correspond to the attribute \"id\", \"class\" or \"role\" of the complex tables');
 
-UPDATE `PARAMETER_ELEMENT` SET `Id_Parameter_Family` = (SELECT `Id_Parameter_Family` FROM `PARAMETER_FAMILY` WHERE `Cd_Parameter_Family` LIKE 'RULES') WHERE `Cd_Parameter_Element` LIKE 'COMPLEX_TABLE_MARKER';
 
 INSERT IGNORE INTO `PARAMETER` (`Id_Parameter_Element`, `Parameter_Value`, `Is_Default`) VALUES
 ((SELECT `Id_Parameter_Element` FROM `PARAMETER_ELEMENT` WHERE `Cd_Parameter_Element` LIKE 'COMPLEX_TABLE_MARKER'), '', b'1');
