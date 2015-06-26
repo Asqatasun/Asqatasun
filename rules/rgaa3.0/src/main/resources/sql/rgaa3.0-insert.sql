2951b84e405034745042b8d7fbcb887bf8dd84ae
Jérôme Kowalczyk <jk@tanaguru.com>
2015-06-26 10:29:09 +0200

    implementation of 1.1.4

--- rules/rgaa3.0/src/main/resources/sql/rgaa3.0-insert.sql
+++ rules/rgaa3.0/src/main/resources/sql/rgaa3.0-insert.sql
@@ -650,6 +650,7 @@ UPDATE TEST SET `No_Process`=b'0' WHERE
 Cd_Test='Rgaa30-1-1-1' OR 
 Cd_Test='Rgaa30-1-1-2' OR 
 Cd_Test='Rgaa30-1-1-3' OR 
+Cd_Test='Rgaa30-1-1-4' OR 
 Cd_Test='Rgaa30-1-3-3' OR 
 Cd_Test='Rgaa30-1-5-1' OR
 Cd_Test='Rgaa30-1-5-2' OR
--- rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule010104Test.java
+++ rules/rgaa3.0/src/test/java/org/tanaguru/rules/rgaa30/Rgaa30Rule010104Test.java
@@ -19,8 +19,13 @@
  */
 package org.tanaguru.rules.rgaa30;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.tanaguru.entity.audit.TestSolution;
 import org.tanaguru.entity.audit.ProcessResult;
+import static org.tanaguru.rules.keystore.AttributeStore.ALT_ATTR;
+import static org.tanaguru.rules.keystore.AttributeStore.SRC_ATTR;
+import org.tanaguru.rules.keystore.HtmlElementStore;
+import static org.tanaguru.rules.keystore.RemarkMessageStore.CHECK_LINK_ASSO_WITH_SERVER_SIDED_IMG_MAP;
 import org.tanaguru.rules.rgaa30.test.Rgaa30RuleImplementationTestCase;
 
 /**
@@ -46,61 +51,46 @@ public class Rgaa30Rule010104Test extends Rgaa30RuleImplementationTestCase {
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa30.Test.1.1.4-1Passed-01");
-//        addWebResource("Rgaa30.Test.1.1.4-2Failed-01");
         addWebResource("Rgaa30.Test.1.1.4-3NMI-01");
-//        addWebResource("Rgaa30.Test.1.1.4-4NA-01");
+        addWebResource("Rgaa30.Test.1.1.4-3NMI-02");
+        addWebResource("Rgaa30.Test.1.1.4-4NA-01");
     }
 
     @Override
     protected void setProcess() {
         //----------------------------------------------------------------------
-        //------------------------------1Passed-01------------------------------
-        //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa30.Test.1.1.4-1Passed-01"), 1);
-
-        //----------------------------------------------------------------------
-        //------------------------------2Failed-01------------------------------
-        //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa30.Test.1.1.4-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-        //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
         ProcessResult processResult = processPageTest("Rgaa30.Test.1.1.4-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_LINK_ASSO_WITH_SERVER_SIDED_IMG_MAP,
+                HtmlElementStore.IMG_ELEMENT,
+                1,
+                new ImmutablePair(ALT_ATTR, "Map"),
+                new ImmutablePair(SRC_ATTR, "mock-image-ismap-presence.jpg"));
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-02---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa30.Test.1.1.4-3NMI-02");
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                CHECK_LINK_ASSO_WITH_SERVER_SIDED_IMG_MAP,
+                HtmlElementStore.INPUT_ELEMENT,
+                1,
+                new ImmutablePair(ALT_ATTR, "Map"),
+                new ImmutablePair(SRC_ATTR, "mock-image-ismap-presence.jpg"));
 
 
         //----------------------------------------------------------------------
         //------------------------------4NA-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa30.Test.1.1.4-4NA-01"));
-    }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa30.Test.1.1.4-3NMI-01").getValue());
+        checkResultIsNotApplicable(processPageTest("Rgaa30.Test.1.1.4-4NA-01"));
     }
 
 }
