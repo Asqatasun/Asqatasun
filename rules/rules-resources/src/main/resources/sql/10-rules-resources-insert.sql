9ab737bd527c1b7f9b474e7db6995fbd09c57e1d
Jerome Kowalczyk <jk@tanaguru.com>
2015-06-27 23:14:45 +0200

    add aria-labelledby and aria-describedby evidence elements in db

--- rules/rules-resources/src/main/resources/sql/10-rules-resources-insert.sql
+++ rules/rules-resources/src/main/resources/sql/10-rules-resources-insert.sql
@@ -49,6 +49,8 @@ INSERT IGNORE INTO `EVIDENCE` (`Id_Evidence`, `Cd_Evidence`, `Description`, `Lon
 (34, 'Default-Language', NULL, NULL),
 (35, 'Current-Language', NULL, NULL),
 (36, 'aria-label', NULL, NULL),
+(37, 'aria-labelledby', NULL, NULL),
+(38, 'aria-describedby', NULL, NULL),
 (1000, 'Url', NULL, NULL),
 (1002, 'H1', NULL, NULL),
 (1003, 'MetaDescription', NULL, NULL);
