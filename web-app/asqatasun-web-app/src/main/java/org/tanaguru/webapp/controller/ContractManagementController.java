f8e297ea9f83954a452dc8b888dc1a085d5ef4ba
Fabrice Gangler <fabrice@asqatasun.org>
2015-11-01 09:48:50 +0100

    change the name in the comments  (END)

--- web-app/asqatasun-web-app/src/main/java/org/tanaguru/webapp/controller/ContractManagementController.java
+++ web-app/asqatasun-web-app/src/main/java/org/tanaguru/webapp/controller/ContractManagementController.java
@@ -4,9 +4,9 @@ package org.tanaguru.webapp.controller;
  * Asqatasun - Automated webpage assessment
  * Copyright (C) 2008-2015  Asqatasun.org
  *
- * This file is part of Tanaguru.
+ * This file is part of Asqatasun.
  *
- * Tanaguru is free software: you can redistribute it and/or modify
+ * Asqatasun is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
