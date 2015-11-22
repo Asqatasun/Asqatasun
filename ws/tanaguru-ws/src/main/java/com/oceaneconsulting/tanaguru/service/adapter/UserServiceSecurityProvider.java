3f21168e6ec5a49ab29b973798ac1a7dc1479d96
koj <jk-dev@meskoj.fr>
2015-11-22 11:43:41 +0100

    rename package to org.asqatasun

--- ws/tanaguru-ws/src/main/java/com/oceaneconsulting/tanaguru/service/adapter/UserServiceSecurityProvider.java
+++
@@ -1,9 +0,0 @@
-package com.oceaneconsulting.tanaguru.service.adapter;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-
-//adapter definition interface for WsUserService 
-public interface UserServiceSecurityProvider extends UserDetailsService {
-
-}
