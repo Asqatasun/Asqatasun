fee30d331590ee7dc6d1572ff40a10aa2fe2af44
Fabrice Gangler <fabrice@asqatasun.org>
2016-03-29 22:54:58 +0200

    remove rules RGAA v2

--- web-app/tgol-resources/src/main/resources/installation/install.sh
+++ web-app/tgol-resources/src/main/resources/installation/install.sh
@@ -305,7 +305,6 @@ create_tables() {
     cd "$PKG_DIR/install/rules/sql"
     my_sql_insert 10-rules-resources-insert.sql
     my_sql_insert accessiweb2.2-insert.sql
-    my_sql_insert rgaa2.2-insert.sql
     my_sql_insert rgaa3.0-insert.sql
     my_sql_insert seo1.0-insert.sql
 }
