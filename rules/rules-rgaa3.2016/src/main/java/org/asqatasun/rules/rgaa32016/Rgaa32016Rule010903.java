819c079cf51e2a677766dd553b493dd4092aeb52
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:20 +0200

    Rgaa3.2016, enabled 1.9.3 from Rgaa3.0_1.9.4: new ID / same content
    ----> enabled in database
    
    rule 1.9.3: same content but new ID (1.9.3 instead of 1.9.4)
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010904.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010904Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010904/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-9-4.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010903.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010903.java
@@ -17,25 +17,38 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.rules.elementselector.ImageElementSelector;
+import static org.asqatasun.rules.keystore.AttributeStore.DATA_ATTR;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.OBJECT_TYPE_IMG_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG;
 
 /**
- * Implementation of the rule 1.9.3 of the referential RGAA 3.2016
+ * Implementation of the rule 1.9.5 of the referential RGAA 3.2016
  * <br/>
- * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-9-3.html">the rule 1.9.3 design page.</a>
- * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-9-3">1.9.3 rule specification</a>
+ * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-9-5.html">the rule 1.9.5 design page.</a>
+ * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-9-5"> 1.9.5 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule010903 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule010903 extends AbstractDetectionPageRuleImplementation {
 
     /**
      * Default constructor
      */
-    public Rgaa32016Rule010903 () {
-        super();
+    public Rgaa32016Rule010903  () {
+        super(
+                new ImageElementSelector(OBJECT_TYPE_IMG_CSS_LIKE_QUERY),
+                // solution when at least one element is found
+                new ImmutablePair(TestSolution.NEED_MORE_INFO,MANUAL_CHECK_ON_ELEMENTS_MSG),
+                // solution when no element is found
+                new ImmutablePair(TestSolution.NOT_APPLICABLE,""),
+                DATA_ATTR
+            );
     }
 
 }
--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -916,3 +916,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-8-4';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-8-5';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-9-3';
+
