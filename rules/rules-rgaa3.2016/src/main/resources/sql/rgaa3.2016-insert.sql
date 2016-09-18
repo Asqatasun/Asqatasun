35c5d9f0777355e8961af09c23e3b54a8ffb762f
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:20 +0200

    Rgaa3.2016, enabled 1.8.5 from Rgaa3.0_1.8.6: new ID / same content
    ----> enabled in database
    
    rule 1.8.5: same content but new ID (1.8.5 instead of 1.8.6)
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010806.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010806Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010806/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-8-6.md

--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -914,3 +914,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-8-3';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-8-4';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-1-8-5';
+
