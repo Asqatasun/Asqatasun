c53a84fa0e38d577e7763df9961359671465bb41
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:44:57 +0200

    Rgaa3.2016, enabled 1.5.2 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 1.5.2: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010502.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010502Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010502/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-5-2.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010502.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010502.java
@@ -17,9 +17,15 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.rules.elementselector.CaptchaElementSelector;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.FORM_BUTTON_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_CAPTCHA_ALTERNATIVE_ACCES_MSG;
 
 /**
  * Implementation of the rule 1.5.2 of the referential RGAA 3.2016
@@ -27,15 +33,24 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-5-2.html">the rule 1.5.2 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-5-2">1.5.2 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule010502 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule010502 extends AbstractDetectionPageRuleImplementation {
 
     /**
      * Default constructor
      */
-    public Rgaa32016Rule010502 () {
-        super();
+    public Rgaa32016Rule010502  () {
+        super(
+                new CaptchaElementSelector(
+                    new SimpleElementSelector(FORM_BUTTON_CSS_LIKE_QUERY)),
+                // solution when at least one element is found
+                TestSolution.NEED_MORE_INFO,
+                // solution when no element is found
+                TestSolution.NOT_APPLICABLE,
+                // manual check message
+                CHECK_CAPTCHA_ALTERNATIVE_ACCES_MSG,
+                null
+            );
     }
 
 }
