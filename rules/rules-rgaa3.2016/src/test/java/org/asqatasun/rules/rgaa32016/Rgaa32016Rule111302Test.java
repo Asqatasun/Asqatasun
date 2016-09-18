b13ece4530e3c2c383bdc602fa067fdbb42e23c0
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:03 +0200

    Rgaa3.2016, enabled 11.13.2 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 11.13.2: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule111302.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule111302Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule111302/*
    /documentation/en/90_Rules/rgaa3.0/11.Forms/Rule-11-13-2.md

--- rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule111302Test.java
+++ rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule111302Test.java
@@ -19,14 +19,16 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.entity.audit.ProcessResult;
+import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
+import org.asqatasun.rules.keystore.HtmlElementStore;
+import org.asqatasun.rules.keystore.RemarkMessageStore;
 
 /**
  * Unit test class for the implementation of the rule 11.13.2 of the referential RGAA 3.2016
  *
- * @author
+ * @author jkowalczyk
  */
 public class Rgaa32016Rule111302Test extends Rgaa32016RuleImplementationTestCase {
 
@@ -40,67 +42,36 @@ public class Rgaa32016Rule111302Test extends Rgaa32016RuleImplementationTestCase
 
     @Override
     protected void setUpRuleImplementationClassName() {
-        setRuleImplementationClassName(
-                "org.asqatasun.rules.rgaa32016.Rgaa32016Rule111302");
+        setRuleImplementationClassName("org.asqatasun.rules.rgaa32016.Rgaa32016Rule111302");
     }
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa32016.Test.11.13.2-1Passed-01");
-//        addWebResource("Rgaa32016.Test.11.13.2-2Failed-01");
-        addWebResource("Rgaa32016.Test.11.13.2-3NMI-01");
-//        addWebResource("Rgaa32016.Test.11.13.2-4NA-01");
+        addWebResource("Rgaa32016.Test.11.13.02-3NMI-01");
+        addWebResource("Rgaa32016.Test.11.13.02-4NA-01");
+
     }
 
     @Override
     protected void setProcess() {
         //----------------------------------------------------------------------
-        //------------------------------1Passed-01------------------------------
-        //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa32016.Test.11.13.2-1Passed-01"), 1);
-
-        //----------------------------------------------------------------------
-        //------------------------------2Failed-01------------------------------
-        //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.13.2-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-        //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.13.2-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-
+        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.13.02-3NMI-01");
+        checkResultIsPreQualified(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG,
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+               
+        
         //----------------------------------------------------------------------
-        //------------------------------4NA-01------------------------------
+        //------------------------------4NA-01----------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.11.13.2-4NA-01"));
+        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.11.13.02-4NA-01"));
+
     }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa32016.Test.11.13.2-3NMI-01").getValue());
-}
 
 }
