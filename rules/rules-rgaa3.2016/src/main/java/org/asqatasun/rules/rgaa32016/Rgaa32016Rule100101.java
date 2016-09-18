a4040268726c331e0d954d44d84cc5a32f250563
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:01 +0200

    Rgaa3.2016, enabled 10.1.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 10.1.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule100101.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule100101Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule100101/*
    /documentation/en/90_Rules/rgaa3.0/10.Presentation_of_information/Rule-10-1-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule100101.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule100101.java
@@ -19,7 +19,12 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.entity.reference.Nomenclature;
+import org.asqatasun.processor.SSPHandler;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.rules.elementselector.MultipleElementSelector;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.PRESENTATION_TAG_DETECTED_MSG;
 
 /**
  * Implementation of the rule 10.1.1 of the referential RGAA 3.2016
@@ -27,15 +32,46 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/10.Presentation_of_information/Rule-10-1-1.html">the rule 10.1.1 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-10-1-1">10.1.1 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule100101 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule100101 extends AbstractDetectionPageRuleImplementation {
 
+    private static final String DEPREC_TAG_NOM = "DeprecatedRepresentationTagsV2";
+    /* the total number of elements */
+    int totalNumberOfElements = 0;
+    
     /**
      * Default constructor
      */
-    public Rgaa32016Rule100101 () {
-        super();
+    public Rgaa32016Rule100101  () {
+        super(
+                new MultipleElementSelector(),
+                // solution when at least one element is found
+                TestSolution.FAILED,
+                // solution when no element is found
+                TestSolution.PASSED,
+                // manual check message
+                PRESENTATION_TAG_DETECTED_MSG,
+                null
+            );
+    }
+
+    @Override
+    protected void select(SSPHandler sspHandler) {
+        // retrieve element from the nomenclature
+        Nomenclature deprecatedHtmlTags = nomenclatureLoaderService.
+                loadByCode(DEPREC_TAG_NOM);
+        // add each element of the nomenclature to the selector
+        MultipleElementSelector mes = (MultipleElementSelector)getElementSelector();
+        for (String deprecatedTag  : deprecatedHtmlTags.getValueList()) {
+            mes.addCssQuery(deprecatedTag.toLowerCase());
+        }
+        totalNumberOfElements = sspHandler.getTotalNumberOfElements();
+        super.select(sspHandler);
+    }
+    
+     @Override
+    public int getSelectionSize () {
+        return totalNumberOfElements;
     }
 
 }
