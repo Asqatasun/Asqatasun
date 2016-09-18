2bbd028b841462f32bf339ac610f26a607ab0baa
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:44:57 +0200

    Rgaa3.2016, enabled 1.6.2 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 1.6.2: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010602.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010602Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010602/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-6-2.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010602.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010602.java
@@ -19,7 +19,18 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.ImageElementSelector;
+import static org.asqatasun.rules.keystore.AttributeStore.DATA_ATTR;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.OBJECT_TYPE_IMG_NOT_IN_LINK_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.asqatasun.rules.keystore.MarkerStore.DECORATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.INFORMATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_DETAILED_DESC_DEFINITION_OF_INFORMATIVE_IMG_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE_AND_DETAILED_DESC_AVAILABILITY_MSG;
 
 /**
  * Implementation of the rule 1.6.2 of the referential RGAA 3.2016
@@ -27,15 +38,42 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-6-2.html">the rule 1.6.2 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-6-2">1.6.2 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule010602 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule010602 extends AbstractMarkerPageRuleImplementation {
 
     /**
      * Default constructor
      */
     public Rgaa32016Rule010602 () {
-        super();
+        super(
+                new ImageElementSelector(OBJECT_TYPE_IMG_NOT_IN_LINK_CSS_LIKE_QUERY),
+
+                // the informative images are part of the scope
+                INFORMATIVE_IMAGE_MARKER, 
+
+                // the decorative images are not part of the scope
+                DECORATIVE_IMAGE_MARKER, 
+
+                // checker for elements identified by marker
+                new ElementPresenceChecker(
+                    // solution when at least one element is found
+                    new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_DETAILED_DESC_DEFINITION_OF_INFORMATIVE_IMG_MSG),
+                    // solution when no element is found
+                    new ImmutablePair(TestSolution.NOT_APPLICABLE,""), 
+                    // evidence elements
+                    TEXT_ELEMENT2,
+                    DATA_ATTR), 
+                
+                // checker for elements not identified by marker
+                new ElementPresenceChecker(
+                    // solution when at least one element is found
+                    new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_NATURE_OF_IMAGE_AND_DETAILED_DESC_AVAILABILITY_MSG),
+                    // solution when no element is found
+                    new ImmutablePair(TestSolution.NOT_APPLICABLE,""), 
+                    // evidence elements
+                    TEXT_ELEMENT2,
+                    DATA_ATTR)
+            );
     }
 
 }
