a0fb23aa2b354d60834ac0148beb0663143dd342
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:02 +0200

    Rgaa3.2016, enabled 11.6.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 11.6.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110601.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110601Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule110601/*
    /documentation/en/90_Rules/rgaa3.0/11.Forms/Rule-11-6-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110601.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110601.java
@@ -19,23 +19,40 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.asqatasun.rules.elementchecker.element.ChildElementPresenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.HtmlElementStore.FIELDSET_ELEMENT;
+import static org.asqatasun.rules.keystore.HtmlElementStore.LEGEND_ELEMENT;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.FIELDSET_WITHOUT_LEGEND_MSG;
 
 /**
  * Implementation of the rule 11.6.1 of the referential RGAA 3.2016
  * <br/>
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/11.Forms/Rule-11-6-1.html">the rule 11.6.1 design page.</a>
- * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-11-6-1">11.6.1 rule specification</a>
+ * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-11-7-1">11.6.1 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule110601 extends AbstractNotTestedRuleImplementation {
-
+public class Rgaa32016Rule110601 extends AbstractPageRuleWithSelectorAndCheckerImplementation {
+    
     /**
-     * Default constructor
+     * Constructor
      */
     public Rgaa32016Rule110601 () {
-        super();
+        super(
+                new SimpleElementSelector(FIELDSET_ELEMENT), 
+                
+                new ChildElementPresenceChecker(
+                    LEGEND_ELEMENT, 
+                        1,
+                    // passed when a child element is found
+                    new ImmutablePair(TestSolution.PASSED,""),    
+                    // failed when no child element is found
+                    new ImmutablePair(TestSolution.FAILED, FIELDSET_WITHOUT_LEGEND_MSG)
+                )
+        );
     }
-
+    
 }
--- rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110601Test.java
+++ rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110601Test.java
@@ -19,14 +19,16 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.entity.audit.ProcessResult;
+import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
+import org.asqatasun.rules.keystore.HtmlElementStore;
+import org.asqatasun.rules.keystore.RemarkMessageStore;
 
 /**
  * Unit test class for the implementation of the rule 11.6.1 of the referential RGAA 3.2016
  *
- * @author
+ * @author jkowalczyk
  */
 public class Rgaa32016Rule110601Test extends Rgaa32016RuleImplementationTestCase {
 
@@ -40,16 +42,15 @@ public class Rgaa32016Rule110601Test extends Rgaa32016RuleImplementationTestCase
 
     @Override
     protected void setUpRuleImplementationClassName() {
-        setRuleImplementationClassName(
-                "org.asqatasun.rules.rgaa32016.Rgaa32016Rule110601");
+        setRuleImplementationClassName("org.asqatasun.rules.rgaa32016.Rgaa32016Rule110601");
     }
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa32016.Test.11.6.1-1Passed-01");
-//        addWebResource("Rgaa32016.Test.11.6.1-2Failed-01");
-        addWebResource("Rgaa32016.Test.11.6.1-3NMI-01");
-//        addWebResource("Rgaa32016.Test.11.6.1-4NA-01");
+        addWebResource("Rgaa32016.Test.11.06.01-1Passed-01");
+        addWebResource("Rgaa32016.Test.11.06.01-2Failed-01");
+        addWebResource("Rgaa32016.Test.11.06.01-4NA-01");
+
     }
 
     @Override
@@ -57,50 +58,26 @@ public class Rgaa32016Rule110601Test extends Rgaa32016RuleImplementationTestCase
         //----------------------------------------------------------------------
         //------------------------------1Passed-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa32016.Test.11.6.1-1Passed-01"), 1);
-
+        checkResultIsPassed(processPageTest("Rgaa32016.Test.11.06.01-1Passed-01"),1);
+        
+        
         //----------------------------------------------------------------------
         //------------------------------2Failed-01------------------------------
         //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.6.1-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
+        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.06.01-2Failed-01");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.FIELDSET_WITHOUT_LEGEND_MSG,
+                HtmlElementStore.FIELDSET_ELEMENT,
+                1);
+               
+        
         //----------------------------------------------------------------------
-        //------------------------------3NMI-01---------------------------------
+        //------------------------------4NA-01----------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa32016.Test.11.6.1-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-
-        //----------------------------------------------------------------------
-        //------------------------------4NA-01------------------------------
-        //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.11.6.1-4NA-01"));
+        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.11.06.01-4NA-01"));
     }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa32016.Test.11.6.1-3NMI-01").getValue());
-}
 
 }
