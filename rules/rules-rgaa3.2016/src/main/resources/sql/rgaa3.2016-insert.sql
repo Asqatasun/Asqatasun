a1b672c149c8c1d78c54abb172d594ed69fab7fc
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:44:59 +0200

    Rgaa3.2016, enabled 6.4.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 6.4.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule060401.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule060401Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule060401/*
    /documentation/en/90_Rules/rgaa3.0/06.Links/Rule-6-4-1.md

--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -738,3 +738,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-6-2-2';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-6-3-1';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-6-4-1';
+
