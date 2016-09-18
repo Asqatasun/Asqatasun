4385a4d936fb28728873eec07805601009deb43e
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:02 +0200

    Rgaa3.2016, enabled 11.8.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 11.8.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110801.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110801Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule110801/*
    /documentation/en/90_Rules/rgaa3.0/11.Forms/Rule-11-8-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110801.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110801.java
@@ -17,9 +17,15 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.SELECT_WITHIN_FORM_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.MANUAL_CHECK_ON_ELEMENTS_MSG;
 
 /**
  * Implementation of the rule 11.8.1 of the referential RGAA 3.2016
@@ -27,15 +33,20 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/11.Forms/Rule-11-8-1.html">the rule 11.8.1 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-11-8-1">11.8.1 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule110801 extends AbstractNotTestedRuleImplementation {
-
+public class Rgaa32016Rule110801 extends AbstractDetectionPageRuleImplementation {
+    
     /**
      * Default constructor
      */
-    public Rgaa32016Rule110801 () {
-        super();
+    public Rgaa32016Rule110801  () {
+        super(
+                new SimpleElementSelector(SELECT_WITHIN_FORM_CSS_LIKE_QUERY),
+                // solution when at least one element is found
+                new ImmutablePair(TestSolution.NEED_MORE_INFO,MANUAL_CHECK_ON_ELEMENTS_MSG),
+                // solution when no element is found
+                new ImmutablePair(TestSolution.NOT_APPLICABLE,"")
+            );
     }
 
 }
--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -810,3 +810,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-6-1';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-7-1';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-8-1';
+
