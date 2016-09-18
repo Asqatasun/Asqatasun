79fce498fa2819288c1dcaf4940993e424f2ebe9
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:44:59 +0200

    Rgaa3.2016, enabled 5.8.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 5.8.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule050801.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule050801Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule050801/*
    /documentation/en/90_Rules/rgaa3.0/05.Tables/Rule-5-8-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050801.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050801.java
@@ -17,9 +17,27 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.processor.SSPHandler;
+import org.asqatasun.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.asqatasun.ruleimplementation.ElementHandler;
+import org.asqatasun.ruleimplementation.ElementHandlerImpl;
+import org.asqatasun.ruleimplementation.TestSolutionHandler;
+import org.asqatasun.rules.elementchecker.ElementChecker;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.DATA_TABLE_MARKUP_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TABLE_ELEMENT;
+import static org.asqatasun.rules.keystore.MarkerStore.COMPLEX_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.DATA_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.PRESENTATION_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.*;
 
 /**
  * Implementation of the rule 5.8.1 of the referential RGAA 3.2016
@@ -27,15 +45,136 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/05.Tables/Rule-5-8-1.html">the rule 5.8.1 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-5-8-1">5.8.1 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule050801 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule050801 extends AbstractMarkerPageRuleImplementation {
 
+    /** 
+     * Tables not identified as presentation table that does not contain
+     * data table markup
+     */
+    private final ElementHandler<Element> notIdentifiedTableWithoutDataTableMarkup = 
+            new ElementHandlerImpl();
+    /** 
+     * Tables identified as presentation table that does not contain
+     * data table markup
+     */
+    private final ElementHandler<Element> presentationTableWithoutDataTableMarkup = 
+            new ElementHandlerImpl();
+    
+    /** The local element counter */
+    private int tableCounter = 0;
+    
     /**
      * Default constructor
      */
     public Rgaa32016Rule050801 () {
-        super();
+        super(
+                new SimpleElementSelector(TABLE_ELEMENT),
+
+                // the presentation tables are not part of the scope
+                new String[]{PRESENTATION_TABLE_MARKER},
+                
+                // the data and complex tables are part of the scope
+                new String[]{DATA_TABLE_MARKER,COMPLEX_TABLE_MARKER},
+
+                // checker for elements identified by marker
+                new ElementPresenceChecker(
+                    // failed when element is found
+                    new ImmutablePair(TestSolution.FAILED, PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG),
+                    // passed when element is not found
+                    new ImmutablePair(TestSolution.NOT_APPLICABLE, "")
+                ),
+                
+                // checker for elements not identified by marker
+                new ElementPresenceChecker(
+                    // nmi when element is found
+                    new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_TABLE_IS_DATA_TABLE_MSG),
+                    // nmi when element is not found
+                    new ImmutablePair(TestSolution.NOT_APPLICABLE, "")
+                )
+            );
     }
 
+    @Override
+    protected void select(SSPHandler sspHandler) {
+        super.select(sspHandler);
+        
+        if (getSelectionWithoutMarkerHandler().isEmpty() && 
+                getSelectionWithMarkerHandler().isEmpty()) {
+            return;
+        }
+        
+        tableCounter = getSelectionWithoutMarkerHandler().get().size() + 
+                       getSelectionWithMarkerHandler().get().size();
+        
+        // extract not identified tables with data table markup
+        extractTableWithDataTableMarkup(
+                    getSelectionWithoutMarkerHandler(), 
+                    notIdentifiedTableWithoutDataTableMarkup);
+        
+        // extract presentation tables with data table markup
+        extractTableWithDataTableMarkup(
+                    getSelectionWithMarkerHandler(), 
+                    presentationTableWithoutDataTableMarkup);
+    }
+    
+    @Override
+    protected void check(
+            SSPHandler sspHandler, 
+            TestSolutionHandler testSolutionHandler) {
+        super.check(sspHandler, testSolutionHandler);
+        ElementChecker ec;
+        if (!notIdentifiedTableWithoutDataTableMarkup.isEmpty()) {
+            ec = new ElementPresenceChecker(
+                        // nmi when element is found
+                        new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_TABLE_IS_PRESENTATION_TABLE_MSG),
+                        // na when element is not found
+                        new ImmutablePair(TestSolution.NOT_APPLICABLE, "")
+            );
+            ec.check(
+                    sspHandler, 
+                    notIdentifiedTableWithoutDataTableMarkup, 
+                    testSolutionHandler);
+        }
+        if (!presentationTableWithoutDataTableMarkup.isEmpty()) {
+            ec = new ElementPresenceChecker(
+                        // passed when element is foundexit
+                        new ImmutablePair(TestSolution.PASSED, ""),
+                        // na when element is not found
+                        new ImmutablePair(TestSolution.NOT_APPLICABLE, "")
+            );
+            ec.check(
+                    sspHandler, 
+                    presentationTableWithoutDataTableMarkup, 
+                    testSolutionHandler);
+        }
+    }
+ 
+    /**
+     * 
+     * @param sspHandler
+     * @param elementHandler 
+     * @param elementHandlerWithoutDataTableMarkup
+     */
+    private void extractTableWithDataTableMarkup(
+                ElementHandler<Element> elementHandler, 
+                ElementHandler<Element> elementHandlerWithoutDataTableMarkup) {
+        
+        Elements elementsWithMarkup = new Elements();
+        
+        for (Element el : elementHandler.get()) {
+            if (el.select(DATA_TABLE_MARKUP_CSS_LIKE_QUERY).size() > 0) {
+                elementsWithMarkup.add(el);
+            } else if (elementHandlerWithoutDataTableMarkup != null) {
+                elementHandlerWithoutDataTableMarkup.add(el);
+            }
+        }
+        elementHandler.clean().addAll(elementsWithMarkup);
+    }
+    
+    @Override
+    public int getSelectionSize () {
+        return tableCounter;
+    }
+    
 }
--- rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050801Test.java
+++ rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule050801Test.java
@@ -19,14 +19,16 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.entity.audit.ProcessResult;
+import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
+import org.asqatasun.rules.keystore.HtmlElementStore;
+import org.asqatasun.rules.keystore.RemarkMessageStore;
 
 /**
  * Unit test class for the implementation of the rule 5.8.1 of the referential RGAA 3.2016
  *
- * @author
+ * @author jkowalczyk
  */
 public class Rgaa32016Rule050801Test extends Rgaa32016RuleImplementationTestCase {
 
@@ -40,16 +42,40 @@ public class Rgaa32016Rule050801Test extends Rgaa32016RuleImplementationTestCase
 
     @Override
     protected void setUpRuleImplementationClassName() {
-        setRuleImplementationClassName(
-                "org.asqatasun.rules.rgaa32016.Rgaa32016Rule050801");
+        setRuleImplementationClassName("org.asqatasun.rules.rgaa32016.Rgaa32016Rule050801");
     }
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa32016.Test.5.8.1-1Passed-01");
-//        addWebResource("Rgaa32016.Test.5.8.1-2Failed-01");
-        addWebResource("Rgaa32016.Test.5.8.1-3NMI-01");
-//        addWebResource("Rgaa32016.Test.5.8.1-4NA-01");
+        addWebResource("Rgaa32016.Test.05.08.01-1Passed-01",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-01",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-02",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-03",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-04",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-05",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-06",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-07",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "id-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-2Failed-08",
+                    createParameter("Rules", "PRESENTATION_TABLE_MARKER", "class-presentation-table"));
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-01");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-02");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-03");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-04");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-05");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-06");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-07");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-08");
+        addWebResource("Rgaa32016.Test.05.08.01-3NMI-09");
+        addWebResource("Rgaa32016.Test.05.08.01-4NA-01");
+
     }
 
     @Override
@@ -57,50 +83,296 @@ public class Rgaa32016Rule050801Test extends Rgaa32016RuleImplementationTestCase
         //----------------------------------------------------------------------
         //------------------------------1Passed-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa32016.Test.5.8.1-1Passed-01"), 1);
-
+        checkResultIsPassed(processPageTest("Rgaa32016.Test.05.08.01-1Passed-01"),1);        
+        
         //----------------------------------------------------------------------
         //------------------------------2Failed-01------------------------------
         //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa32016.Test.5.8.1-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
+        ProcessResult processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-01");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-02------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-02");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-03------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-03");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-04------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-04");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-05------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-05");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-06------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-06");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-07------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-07");
+        checkResultIsFailed(processResult, 1, 1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);        
 
+        //----------------------------------------------------------------------
+        //------------------------------2Failed-08------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-2Failed-08");
+        checkResultIsFailed(processResult, 16, 15);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                3);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                4);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                5);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                6);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.FAILED,
+                RemarkMessageStore.PRESENTATION_TABLE_WITH_FORBIDDEN_MARKUP_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                7);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                8);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                9);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                10);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                11);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                12);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                13);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                14);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_PRESENTATION_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                15);
+        
         //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa32016.Test.5.8.1-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-01");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_PRESENTATION_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-02---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-02");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-03---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-03");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-04---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-04");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-05---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-05");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-06---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-06");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-07---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-07");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-08---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-08");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CHECK_TABLE_IS_DATA_TABLE_MSG,
+                HtmlElementStore.TABLE_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-09---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.05.08.01-3NMI-09");
+        checkResultIsPreQualified(processResult, 2,  2);
 
         //----------------------------------------------------------------------
         //------------------------------4NA-01------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.5.8.1-4NA-01"));
+        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.05.08.01-4NA-01"));
+
+
     }
-
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa32016.Test.5.8.1-3NMI-01").getValue());
-}
 
 }
