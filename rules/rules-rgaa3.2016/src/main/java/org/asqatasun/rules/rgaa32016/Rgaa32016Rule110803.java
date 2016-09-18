1ed48a5a4f553535e90b18b8c2790834d4c92b69
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:02 +0200

    Rgaa3.2016, enabled 11.8.3 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 11.8.3: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110803.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110803Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule110803/*
    /documentation/en/90_Rules/rgaa3.0/11.Forms/Rule-11-8-3.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110803.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110803.java
@@ -17,9 +17,16 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.asqatasun.rules.elementchecker.pertinence.AttributePertinenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.OPTGROUP_WITHIN_SELECT_WITH_LABEL_ATTR_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.AttributeStore.LABEL_ATTR;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_OPTGROUP_LABEL_PERTINENCE_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.NOT_PERTINENT_OPTGROUP_LABEL_MSG;
 
 /**
  * Implementation of the rule 11.8.3 of the referential RGAA 3.2016
@@ -27,15 +34,30 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/11.Forms/Rule-11-8-3.html">the rule 11.8.3 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-11-8-3">11.8.3 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule110803 extends AbstractNotTestedRuleImplementation {
-
+public class Rgaa32016Rule110803 extends AbstractPageRuleWithSelectorAndCheckerImplementation {
+    
     /**
      * Default constructor
      */
-    public Rgaa32016Rule110803 () {
-        super();
+    public Rgaa32016Rule110803  () {
+        super(
+                new SimpleElementSelector(OPTGROUP_WITHIN_SELECT_WITH_LABEL_ATTR_CSS_LIKE_QUERY),
+                new AttributePertinenceChecker(
+                    LABEL_ATTR,
+                    // check emptiness
+                    true, 
+                    // no comparison with other attributes
+                    null, 
+                    // no comparison with blacklist
+                    null, 
+                    //  message associated with element when not pertinent
+                    NOT_PERTINENT_OPTGROUP_LABEL_MSG, 
+                    //message associated with element when pertinence cannot be determined
+                    CHECK_OPTGROUP_LABEL_PERTINENCE_MSG,
+                    // evidence elements
+                    LABEL_ATTR)
+            );
     }
 
 }
--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -814,3 +814,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-8-1';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-8-2';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-8-3';
+
