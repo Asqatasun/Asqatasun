d99adf3aa322fbb2de1844c5c76a8c19f1d131c7
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:04 +0200

    Rgaa3.2016, enabled 12.3.2 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 12.3.2: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule120302.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule120302Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule120302/*
    /documentation/en/90_Rules/rgaa3.0/12.Navigation/Rule-12-3-2.md

--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -848,3 +848,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-12-2-2';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-12-3-1';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-12-3-2';
+
