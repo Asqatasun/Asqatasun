7bce6d622f4dd172916d18eda2f3e9035949871f
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:02 +0200

    Rgaa3.2016, enabled 11.7.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 11.7.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110701.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule110701Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule110701/*
    /documentation/en/90_Rules/rgaa3.0/11.Forms/Rule-11-7-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110701.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule110701.java
@@ -17,9 +17,16 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.ruleimplementation.AbstractPageRuleWithSelectorAndCheckerImplementation;
+import org.asqatasun.rules.elementchecker.pertinence.TextPertinenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.LEGEND_WITHIN_FIELDSET_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_LEGEND_PERTINENCE_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.NOT_PERTINENT_LEGEND_MSG;
 
 /**
  * Implementation of the rule 11.7.1 of the referential RGAA 3.2016
@@ -27,15 +34,26 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/11.Forms/Rule-11-7-1.html">the rule 11.7.1 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-11-7-1">11.7.1 rule specification</a>
  *
- * @author
  */
-public class Rgaa32016Rule110701 extends AbstractNotTestedRuleImplementation {
+public class Rgaa32016Rule110701  extends AbstractPageRuleWithSelectorAndCheckerImplementation {
 
-    /**
-     * Default constructor
-     */
-    public Rgaa32016Rule110701 () {
-        super();
+    public Rgaa32016Rule110701  () {
+        super (
+                 new SimpleElementSelector(LEGEND_WITHIN_FIELDSET_CSS_LIKE_QUERY), 
+                 new TextPertinenceChecker(
+                    // check emptiness
+                    true, 
+                    // no comparison with other attributes
+                    null, 
+                    // no comparison with blacklist
+                    null, 
+                    //  message associated with element when not pertinent
+                    NOT_PERTINENT_LEGEND_MSG, 
+                    //message associated with element when pertinence cannot be determined
+                    CHECK_LEGEND_PERTINENCE_MSG,
+                    // evidence elements
+                    TEXT_ELEMENT2)
+            );
     }
 
 }
--- rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
+++ rules/rules-rgaa3.2016/src/main/resources/sql/rgaa3.2016-insert.sql
@@ -808,3 +808,5 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-5-1';
 
 UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-6-1';
 
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa32016-11-7-1';
+
