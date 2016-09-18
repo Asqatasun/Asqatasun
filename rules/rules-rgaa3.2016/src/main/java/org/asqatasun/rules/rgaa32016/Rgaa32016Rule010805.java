c68fb994bb837cef888225cdb7c5feaad2fcbec1
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:20 +0200

    Rgaa3.2016, enabled 1.8.5 from Rgaa3.0_1.8.6: new ID / same content
    ----> enabled in database
    
    rule 1.8.5: same content but new ID (1.8.5 instead of 1.8.6)
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010806.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010806Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010806/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-8-6.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010805.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010805.java
@@ -19,23 +19,44 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import static org.asqatasun.entity.audit.TestSolution.NEED_MORE_INFO;
+import static org.asqatasun.entity.audit.TestSolution.NOT_APPLICABLE;
+import org.asqatasun.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.ImageElementSelector;
+import static org.asqatasun.rules.keystore.HtmlElementStore.CANVAS_ELEMENT;
+import static org.asqatasun.rules.keystore.MarkerStore.DECORATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.INFORMATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE_AND_TEXT_STYLED_PRESENCE_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_TEXT_STYLED_PRESENCE_OF_INFORMATIVE_IMG_MSG;
 
 /**
  * Implementation of the rule 1.8.5 of the referential RGAA 3.2016
- * <br/>
+ *
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-8-5.html">the rule 1.8.5 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-8-5">1.8.5 rule specification</a>
- *
- * @author
  */
-public class Rgaa32016Rule010805 extends AbstractNotTestedRuleImplementation {
+
+public class Rgaa32016Rule010805 extends AbstractMarkerPageRuleImplementation {
 
     /**
      * Default constructor
      */
-    public Rgaa32016Rule010805 () {
-        super();
+    public Rgaa32016Rule010805  () {
+        super(
+                new ImageElementSelector(CANVAS_ELEMENT),
+                INFORMATIVE_IMAGE_MARKER,
+                DECORATIVE_IMAGE_MARKER,
+                new ElementPresenceChecker(
+                        new ImmutablePair(NEED_MORE_INFO, CHECK_TEXT_STYLED_PRESENCE_OF_INFORMATIVE_IMG_MSG),
+                        new ImmutablePair(NOT_APPLICABLE, "")
+                ),
+                new ElementPresenceChecker(
+                        new ImmutablePair(NEED_MORE_INFO, CHECK_NATURE_OF_IMAGE_AND_TEXT_STYLED_PRESENCE_MSG),
+                        new ImmutablePair(NOT_APPLICABLE, "")
+                )
+            );
     }
 
 }
