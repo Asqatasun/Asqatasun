78784f1bd478666acb1977ce89b219383983f957
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:18 +0200

    Rgaa3.2016, enabled 1.3.12 from Rgaa3.0_1.3.8: new ID / same content
    ----> enabled in database
    
    rule 1.3.12: same content but new ID (1.3.12 instead of 1.3.8)
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010308.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule010308Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule010308/*
    /documentation/en/90_Rules/rgaa3.0/01.Images/Rule-1-3-8.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010312.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule010312.java
@@ -19,23 +19,69 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.asqatasun.rules.elementchecker.ElementChecker;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.CANVAS_NOT_IN_LINK_WITH_NOT_EMPTY_CONTENT_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.asqatasun.rules.keystore.MarkerStore.DECORATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.INFORMATIVE_IMAGE_MARKER;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_AT_RESTITUTION_OF_ALTERNATIVE_OF_INFORMATIVE_IMAGE_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_IMAGE_AND_AT_RESTITUTION_OF_ALTERNATIVE_MSG;
 
 /**
  * Implementation of the rule 1.3.12 of the referential RGAA 3.2016
- * <br/>
+ *
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/01.Images/Rule-1-3-12.html">the rule 1.3.12 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-1-3-12">1.3.12 rule specification</a>
- *
- * @author
  */
-public class Rgaa32016Rule010312 extends AbstractNotTestedRuleImplementation {
+
+public class Rgaa32016Rule010312 extends AbstractMarkerPageRuleImplementation {
 
     /**
      * Default constructor
      */
     public Rgaa32016Rule010312 () {
-        super();
+        super(
+                // the informative images are part of the scope
+                INFORMATIVE_IMAGE_MARKER,
+                // the decorative images are not part of the scope
+                DECORATIVE_IMAGE_MARKER);
+
+        setElementSelector(new SimpleElementSelector(CANVAS_NOT_IN_LINK_WITH_NOT_EMPTY_CONTENT_CSS_LIKE_QUERY));
+        setMarkerElementChecker(getMarkerElementChecker());
+        setRegularElementChecker(getLocalRegularElementChecker());
+    }
+
+    /**
+     *
+     * @return the checker user for marked elements
+     */
+    private ElementChecker getMarkerElementChecker () {
+        ElementChecker ec = new ElementPresenceChecker(
+                new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_AT_RESTITUTION_OF_ALTERNATIVE_OF_INFORMATIVE_IMAGE_MSG),
+                new ImmutablePair(TestSolution.NOT_APPLICABLE, ""),
+                // evidence element
+                TEXT_ELEMENT2
+        );
+        return ec;
+    }
+    
+    /**
+     *
+     * @return the checker user for marked elements
+     */
+    private ElementChecker getLocalRegularElementChecker () {
+        ElementChecker ec = new ElementPresenceChecker(
+                new ImmutablePair(TestSolution.NEED_MORE_INFO, CHECK_NATURE_OF_IMAGE_AND_AT_RESTITUTION_OF_ALTERNATIVE_MSG),
+                new ImmutablePair(TestSolution.NOT_APPLICABLE, ""),
+                // evidence element
+                TEXT_ELEMENT2
+        );
+        return ec;
     }
 
 }
