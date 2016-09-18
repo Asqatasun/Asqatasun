4b3793406e05faac76e4c5d522fe905630a7d24b
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:44:59 +0200

    Rgaa3.2016, enabled 7.4.1 from Rgaa3.0: same ID / same content
    ----> enabled in database
    
    rule 7.4.1: same ID and same content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule070401.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule070401Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule070401/*
    /documentation/en/90_Rules/rgaa3.0/07.Scripts/Rule-7-4-1.md

--- rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule070401Test.java
+++ rules/rules-rgaa3.2016/src/test/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule070401Test.java
@@ -19,14 +19,16 @@
  */
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.entity.audit.ProcessResult;
+import org.asqatasun.entity.audit.TestSolution;
 import org.asqatasun.rules.rgaa32016.test.Rgaa32016RuleImplementationTestCase;
+import org.asqatasun.rules.keystore.HtmlElementStore;
+import org.asqatasun.rules.keystore.RemarkMessageStore;
 
 /**
  * Unit test class for the implementation of the rule 7.4.1 of the referential RGAA 3.2016
  *
- * @author
+ * @author jkowalczyk
  */
 public class Rgaa32016Rule070401Test extends Rgaa32016RuleImplementationTestCase {
 
@@ -40,67 +42,151 @@ public class Rgaa32016Rule070401Test extends Rgaa32016RuleImplementationTestCase
 
     @Override
     protected void setUpRuleImplementationClassName() {
-        setRuleImplementationClassName(
-                "org.asqatasun.rules.rgaa32016.Rgaa32016Rule070401");
+        setRuleImplementationClassName("org.asqatasun.rules.rgaa32016.Rgaa32016Rule070401");
     }
 
     @Override
     protected void setUpWebResourceMap() {
-//        addWebResource("Rgaa32016.Test.7.4.1-1Passed-01");
-//        addWebResource("Rgaa32016.Test.7.4.1-2Failed-01");
-        addWebResource("Rgaa32016.Test.7.4.1-3NMI-01");
-//        addWebResource("Rgaa32016.Test.7.4.1-4NA-01");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-01");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-02");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-03");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-04");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-05");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-06");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-07");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-08");
+        addWebResource("Rgaa32016.Test.07.04.01-3NMI-09");
+
     }
 
     @Override
     protected void setProcess() {
         //----------------------------------------------------------------------
-        //------------------------------1Passed-01------------------------------
-        //----------------------------------------------------------------------
-//        checkResultIsPassed(processPageTest("Rgaa32016.Test.7.4.1-1Passed-01"), 1);
-
-        //----------------------------------------------------------------------
-        //------------------------------2Failed-01------------------------------
-        //----------------------------------------------------------------------
-//        ProcessResult processResult = processPageTest("Rgaa32016.Test.7.4.1-2Failed-01");
-//        checkResultIsFailed(processResult, 1, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.FAILED,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-        //----------------------------------------------------------------------
         //------------------------------3NMI-01---------------------------------
         //----------------------------------------------------------------------
-        ProcessResult processResult = processPageTest("Rgaa32016.Test.7.4.1-3NMI-01");
-        checkResultIsNotTested(processResult); // temporary result to make the result buildable before implementation
-//        checkResultIsPreQualified(processResult, 2, 1);
-//        checkRemarkIsPresent(
-//                processResult,
-//                TestSolution.NEED_MORE_INFO,
-//                "#MessageHere",
-//                "#CurrentElementHere",
-//                1,
-//                new ImmutablePair("#ExtractedAttributeAsEvidence", "#ExtractedAttributeValue"));
-
-
+        ProcessResult processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-01");
+        checkResultIsPreQualified(processResult, 0,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                getMessageKey(RemarkMessageStore.NO_PATTERN_DETECTED_MSG),
+                HtmlElementStore.SELECT_ELEMENT,
+                1); 
+        
         //----------------------------------------------------------------------
-        //------------------------------4NA-01------------------------------
+        //------------------------------3NMI-02---------------------------------
         //----------------------------------------------------------------------
-//        checkResultIsNotApplicable(processPageTest("Rgaa32016.Test.7.4.1-4NA-01"));
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-02");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CONTEXT_CHANGED_SCRIPT_MSG,
+                HtmlElementStore.SELECT_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-03---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-03");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CONTEXT_CHANGED_SCRIPT_MSG,
+                HtmlElementStore.FORM_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-04---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-04");
+        checkResultIsPreQualified(processResult, 1,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CONTEXT_CHANGED_SCRIPT_MSG,
+                HtmlElementStore.SELECT_ELEMENT,
+                1);        
+        
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-05---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-05");
+        checkResultIsPreQualified(processResult, 0,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                getMessageKey(RemarkMessageStore.NO_PATTERN_DETECTED_MSG),
+                HtmlElementStore.FORM_ELEMENT,
+                1);        
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-06---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-06");
+        checkResultIsPreQualified(processResult, 0,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                getMessageKey(RemarkMessageStore.NO_PATTERN_DETECTED_MSG),
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-07---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-07");
+        checkResultIsPreQualified(processResult, 0,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                getMessageKey(RemarkMessageStore.NO_PATTERN_DETECTED_MSG),
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-08---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-08");
+        checkResultIsPreQualified(processResult, 0,  1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                getMessageKey(RemarkMessageStore.NO_PATTERN_DETECTED_MSG),
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+        
+        //----------------------------------------------------------------------
+        //------------------------------3NMI-09---------------------------------
+        //----------------------------------------------------------------------
+        processResult = processPageTest("Rgaa32016.Test.07.04.01-3NMI-09");
+        checkResultIsPreQualified(processResult, 2,  2);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CONTEXT_CHANGED_SCRIPT_MSG,
+                HtmlElementStore.FORM_ELEMENT,
+                1);
+        checkRemarkIsPresent(
+                processResult,
+                TestSolution.NEED_MORE_INFO,
+                RemarkMessageStore.CONTEXT_CHANGED_SCRIPT_MSG,
+                HtmlElementStore.SELECT_ELEMENT,
+                2);        
     }
 
-    @Override
-    protected void setConsolidate() {
-
-        // The consolidate method can be removed when real implementation is done.
-        // The assertions are automatically tested regarding the file names by 
-        // the abstract parent class
-        assertEquals(TestSolution.NOT_TESTED,
-                consolidate("Rgaa32016.Test.7.4.1-3NMI-01").getValue());
-}
-
+    /**
+     * 
+     * @param msg
+     * @return the message suffixed with the test key identifier
+     */
+    private String getMessageKey(String msg) {
+        StringBuilder strb = new StringBuilder(msg);
+        strb.append("_");
+        strb.append(getName());
+        return strb.toString();
+    }
+    
 }
