5ae1cd57d46166ab81d6825cd64a687028472067
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:28 +0200

    Rgaa3.2016_13.2.1 from Rgaa3.0: same ID / new content
    ----> not enabled in database
    
    rule 13.2.1: same ID and new content
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule130201.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule130201Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule130201/*
    /documentation/en/90_Rules/rgaa3.0/13.Consultation/Rule-13-2-1.md

--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule130201.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule130201.java
@@ -17,9 +17,22 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.processor.SSPHandler;
+import org.asqatasun.ruleimplementation.AbstractDetectionPageRuleImplementation;
+import org.asqatasun.ruleimplementation.TestSolutionHandler;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import static org.asqatasun.rules.keystore.AttributeStore.TITLE_ATTR;
+import static org.asqatasun.rules.keystore.CssLikeQueryStore.LINK_WITH_TARGET_ATTR_CSS_LIKE_QUERY;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_JS_PROMPT_A_NEW_WINDOW_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_USER_IS_WARNED_WHEN_NEW_WINDOW_OPEN_MSG;
+import org.asqatasun.rules.textbuilder.LinkTextElementBuilder;
 
 /**
  * Implementation of the rule 13.2.1 of the referential RGAA 3.2016
@@ -27,15 +40,35 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/13.Consultation/Rule-13-2-1.html">the rule 13.2.1 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-13-2-1">13.2.1 rule specification</a>
  *
- * @author
+ * @author jkowalczyk
  */
-public class Rgaa32016Rule130201 extends AbstractNotTestedRuleImplementation {
+
+public class Rgaa32016Rule130201 extends AbstractDetectionPageRuleImplementation {
 
     /**
      * Default constructor
      */
-    public Rgaa32016Rule130201 () {
-        super();
+    public Rgaa32016Rule130201  () {
+        super(
+                new SimpleElementSelector(LINK_WITH_TARGET_ATTR_CSS_LIKE_QUERY),
+                // solution when at least one element is found
+                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_USER_IS_WARNED_WHEN_NEW_WINDOW_OPEN_MSG),
+                // solution when no element is found
+                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_JS_PROMPT_A_NEW_WINDOW_MSG),
+                // evidence elements
+                TEXT_ELEMENT2,
+                TITLE_ATTR
+                );
+    }
+
+    @Override
+    protected void check(
+                SSPHandler sspHandler, 
+                TestSolutionHandler testSolutionHandler) {
+
+        ElementPresenceChecker epc = getElementPresenceChecker();
+        epc.setTextElementBuilder(new LinkTextElementBuilder());
+        epc.check(sspHandler, getElements(), testSolutionHandler);
     }
 
 }
