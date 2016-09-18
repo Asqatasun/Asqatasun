b58bc26c2b1fae387248f5dc57740dbdea6ffb5d
Fabrice Gangler <fabrice@asqatasun.org>
2016-09-18 17:45:20 +0200

    Rgaa3.2016, enabled 6.3.3 from Rgaa3.0_6.3.4: new ID / same content
    ----> enabled in database
    
    rule 6.3.3: same content but new ID (6.3.3 instead of 6.3.4)
    
    source changelog:
    http://references.modernisation.gouv.fr/rgaa/changelog.html
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule060304.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule060304Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule060304/*
    /documentation/en/90_Rules/rgaa3.0/06.Links/Rule-6-3-4.md

--- documentation/en/90_Rules/rgaa3.2016/06.Links/Rule-6-3-3.md
+++ documentation/en/90_Rules/rgaa3.2016/06.Links/Rule-6-3-3.md
@@ -1,8 +1,7 @@
 # RGAA 3.2016 - Rule 6.3.3
 
 ## Summary
-No-check rule
-
+This test consists in checking whether the text of each combined link is enough explicit to understand the purpose and the target out of its context.
 
 ## Business description
 
@@ -18,28 +17,80 @@ No-check rule
 ### Level
 **AAA**
 
-
 ## Technical description
 
 ### Scope
 **Page**
 
 ### Decision level
-@@@TODO
-
+**Semi-Decidable**
 
 ## Algorithm
 
 ### Selection
-None
+
+##### Set1
+
+All the `<a>` tags with a `"href"` attribute, with children (a[href]:has(*) )
+
+##### Set2
+
+All the elements of **Set1** with own text or with more than 1 child
+or with only one child not of type `<img>`, `<canvas>`, `<svg>` or `<object>` (where "img ,
+object[type^=image], object[data^=data:image], object[data$=png],
+object[data$=jpeg], object[data$=jpg],object[data$=bmp],
+object[data$=gif], canvas, svg" returns empty)
+
+##### Set3
+
+All the elements of **Set2** with a not empty text.
 
 ### Process
-None
+
+##### Test1
+
+For each element of **Set3**, we check whether the link content doesn't belong to the text link blacklist.
+
+For each element returning false in **Test1**, raise a MessageA, raise a MessageB instead.
+
+##### Test2
+
+For each element of **Set3**, we check whether the link content doesn't only contain non alphanumeric characters.
+
+For each element returning false in **Test2**, raise a MessageA, raise a MessageB instead.
+
+##### MessageA : Unexplicit Link
+
+-   code : UnexplicitLink
+-   status: Failed
+-   parameter : link text, `"title"` attribute, snippet
+-   present in source : yes
+
+##### MessageB : Check link without context pertinence
+
+-   code : CheckLinkWithoutContextPertinence
+-   status: Need More Info
+-   parameter : link text, `"title"` attribute, snippet
+-   present in source : yes
 
 ### Analysis
 
-#### No Tested
-In all cases
+##### Not Applicable
+
+The page has no combined link (**Set1** is empty)
+
+##### Failed
+
+At least one combined link has a text content which is blacklisted or that only contains non alphanumerical characters (**Test1** OR **Test2** returns false for at least one element)
+
+##### Pre-Qualified
+
+In all other cases
+
+## Notes
+
+All the links that have children different from `<img>`, `<canvas>`, `<svg>` or `<object>`, are considered as combined links.
+
 
 
 ##  TestCases
--- rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule060303.java
+++ rules/rules-rgaa3.2016/src/main/java/org/asqatasun/rules/rgaa32016/Rgaa32016Rule060303.java
@@ -17,9 +17,31 @@
  *
  * Contact us by mail: asqatasun AT asqatasun DOT org
  */
+
 package org.asqatasun.rules.rgaa32016;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.apache.commons.lang3.StringUtils;
+import org.jsoup.nodes.Element;
+import org.asqatasun.entity.audit.ProcessRemark;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.processor.SSPHandler;
+import org.asqatasun.ruleimplementation.ElementHandler;
+import org.asqatasun.ruleimplementation.ElementHandlerImpl;
+import org.asqatasun.ruleimplementation.TestSolutionHandler;
+import org.asqatasun.ruleimplementation.TestSolutionHandlerImpl;
+import org.asqatasun.ruleimplementation.link.AbstractLinkRuleImplementation;
+import org.asqatasun.rules.elementchecker.ElementChecker;
+import org.asqatasun.rules.elementchecker.link.LinkPertinenceChecker;
+import org.asqatasun.rules.elementchecker.pertinence.TextPertinenceChecker;
+import org.asqatasun.rules.elementselector.CompositeLinkElementSelector;
+import static org.asqatasun.rules.keystore.AttributeStore.TITLE_ATTR;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TEXT_ELEMENT2;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_LINK_PERTINENCE_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.UNEXPLICIT_LINK_MSG;
+import org.asqatasun.rules.textbuilder.TextAttributeOfElementBuilder;
+import org.asqatasun.service.ProcessRemarkService;
 
 /**
  * Implementation of the rule 6.3.3 of the referential RGAA 3.2016
@@ -27,15 +49,156 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about the implementation, refer to <a href="http://doc.asqatasun.org/en/90_Rules/rgaa3.2016/06.Links/Rule-6-3-3.html">the rule 6.3.3 design page.</a>
  * @see <a href="http://references.modernisation.gouv.fr/rgaa-accessibilite/criteres.html#test-6-3-3">6.3.3 rule specification</a>
  *
- * @author
+ * @author jkowalczyk
  */
-public class Rgaa32016Rule060303 extends AbstractNotTestedRuleImplementation {
 
+public class Rgaa32016Rule060303 extends AbstractLinkRuleImplementation {
+
+    /* the link text blacklist nomenclature name*/ 
+    private static final String LINK_TEXT_BL_NOM_NAME = "LinkTextBlacklist";
+    
+    /* the checker of link title pertinence*/
+    private final ElementChecker titlePertinenceElementChecker = 
+            new TextPertinenceChecker(
+                new TextAttributeOfElementBuilder(TITLE_ATTR),
+                // no emptiness check
+                false, 
+                // no comparison with other attribute
+                null, 
+                // blacklist to compare with
+                LINK_TEXT_BL_NOM_NAME, 
+                // message when not pertinent
+                UNEXPLICIT_LINK_MSG, 
+                // message when pertinence needs to be manually checked
+                CHECK_LINK_PERTINENCE_MSG, 
+                // evidence elements
+                TEXT_ELEMENT2,
+                TITLE_ATTR);
+    /* local collection of process remarks*/
+    private final Collection<ProcessRemark> remarks = new ArrayList<>();
+    /* local instance of ProcessRemarkService*/
+    ProcessRemarkService  prs;
+    
     /**
      * Default constructor
      */
-    public Rgaa32016Rule060303 () {
-        super();
+    public Rgaa32016Rule060303  () {
+        // context is not taken into consideration 
+        super(new CompositeLinkElementSelector(false, false), 
+              new LinkPertinenceChecker(
+                    // not pertinent solution 
+                    TestSolution.FAILED,
+                    // not pertinent message
+                    UNEXPLICIT_LINK_MSG,
+                    // manual check message
+                    CHECK_LINK_PERTINENCE_MSG,
+                    // evidence elements
+                    TEXT_ELEMENT2,
+                    TITLE_ATTR
+              ),
+              null);
+    }
+
+    @Override
+    protected void check(
+            SSPHandler sspHandler, 
+            TestSolutionHandler testSolutionHandler) {
+        if (getLinkElementSelector().isEmpty()) {
+            testSolutionHandler.addTestSolution(TestSolution.NOT_APPLICABLE);
+            return;
+        }
+        
+        prs = sspHandler.getProcessRemarkService();
+        setServicesToChecker(titlePertinenceElementChecker);
+        
+        if (! getLinkElementSelector().getDecidableElements().isEmpty()) {
+            setServicesToChecker(getDecidableElementsChecker());
+            for (Element el : getLinkElementSelector().getDecidableElements().get()) {
+                testSolutionHandler.addTestSolution(testLink(sspHandler, el));
+            }
+        }
+        // reset service and aggregate all the remarks collected locally
+        // for further save
+        prs.resetService();
+        prs.getRemarkList().addAll(remarks);
+    }
+    
+    /**
+     * 
+     * @param sspHandler
+     * @param el
+     * @return 
+     */
+    private TestSolution testLink(SSPHandler sspHandler, Element el) {
+        ElementHandler<Element> elHandler = new ElementHandlerImpl(el);
+        TestSolutionHandler tsHandler = new TestSolutionHandlerImpl();
+        
+        // reset the service for each tested element. We use the collection 
+        // handled by the service to feed the local collection of remarks
+        prs.resetService();
+        
+        // check the pertinence of the link
+        getDecidableElementsChecker().check(
+                    sspHandler, 
+                    elHandler, 
+                    tsHandler);
+        
+        // get the processRemark for eventually override it with the result
+        // returned by the title pertinence checker
+        ProcessRemark remark = prs.getRemarkList().iterator().next();
+        
+        // we check the pertinence of the title if and only if the result is 
+        // failed. This checker may eventually change the result from failed
+        // to nmi but the inverse is impossible.
+        if (tsHandler.getTestSolution().equals(TestSolution.FAILED)) {
+            
+            // check the pertinence of the title of the link
+            String linkText = getDecidableElementsChecker().
+                                  getTextElementBuilder().
+                                      buildTextFromElement(el);
+            
+            if (testTitleAttributeLink(sspHandler, el, linkText).
+                        equals(TestSolution.NEED_MORE_INFO)) {
+                //override result (evidence element have already been collected
+                remark.setIssue(TestSolution.NEED_MORE_INFO);
+                remark.setMessageCode(CHECK_LINK_PERTINENCE_MSG);
+                remarks.add(remark);
+                return TestSolution.NEED_MORE_INFO;
+            }
+        }
+
+        remarks.add(remark);
+        
+        return tsHandler.getTestSolution();
+    }
+    
+    /**
+     * 
+     * @param sspHandler
+     * @param el
+     * @param linkText
+     * @return 
+     */
+    private TestSolution testTitleAttributeLink(
+            SSPHandler sspHandler, 
+            Element el, 
+            String linkText) {
+        // if the current has no title or has an empty title or has a title 
+        // content identical to the link text, returns not applicable.
+        if (!el.hasAttr(TITLE_ATTR)) {
+            return TestSolution.NOT_APPLICABLE;
+        }
+        String attrValue=el.attr(TITLE_ATTR);
+        if (StringUtils.isBlank(attrValue)) {
+            return TestSolution.NOT_APPLICABLE;
+        }
+        if (StringUtils.equalsIgnoreCase(attrValue, linkText)) {
+            return TestSolution.NOT_APPLICABLE;
+        }
+        ElementHandler<Element> elHandler = new ElementHandlerImpl(el);
+        TestSolutionHandler tsHandler = new TestSolutionHandlerImpl();
+        titlePertinenceElementChecker.check(sspHandler, elHandler, tsHandler);
+        return tsHandler.getTestSolution();
     }
 
 }
