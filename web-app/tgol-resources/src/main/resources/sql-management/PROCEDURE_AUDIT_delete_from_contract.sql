5f8fc16500c0e55a662676b868ac87273b6a88b8
Matthieu FAURE <mfaure@asqatasun.org>
2019-01-28 17:00:09 +0100

    SQL: refactored procedures for AUDIT deletion

---
+++ web-app/tgol-resources/src/main/resources/sql-management/PROCEDURE_AUDIT_delete_from_contract.sql
@@ -0,0 +1,41 @@
+-- -----------------------------------------------------------------
+-- Delete audits of a given contract
+--   Contract is identified by its id
+--
+-- /!\ depends on: delete_audit_from_id
+-- -----------------------------------------------------------------
+DROP PROCEDURE IF EXISTS clean_up_audit_from_contract;
+
+DELIMITER |
+CREATE
+    DEFINER =`asqatasun`@`localhost` PROCEDURE `clean_up_audit_from_contract`
+    (
+        IN contractId INT
+    )
+BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE auditId BIGINT(20);
+    DECLARE audits CURSOR FOR SELECT
+                                  a.Id_Audit
+                              FROM
+                                  WEB_RESOURCE AS wr
+                                      LEFT JOIN AUDIT AS a ON (wr.Id_Audit = a.Id_Audit)
+                                      LEFT JOIN TGSI_ACT_WEB_RESOURCE AS taw
+                                                ON (taw.WEB_RESOURCE_Id_Web_Resource = wr.Id_Web_Resource)
+                                      LEFT JOIN TGSI_ACT AS ta ON (ta.Id_Act = taw.ACT_Id_Act)
+                              WHERE
+                                  ta.CONTRACT_Id_Contract = contractId;
+    DECLARE CONTINUE HANDLER FOR SQLSTATE '02000' SET done = 1;
+
+    OPEN audits;
+    LOOP1:
+        LOOP
+            FETCH audits INTO auditId;
+            IF done = 1 THEN
+                LEAVE LOOP1;
+            END IF;
+            CALL delete_audit_from_id(auditId);
+        END LOOP LOOP1;
+
+END |
+DELIMITER ;
