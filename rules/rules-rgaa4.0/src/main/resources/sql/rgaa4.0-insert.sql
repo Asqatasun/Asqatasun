9c7f8bb8ca29ccf1370bc9cf20b3308cd371a103
Fabrice Gangler <fabrice.gangler@adullact.org>
2020-09-25 06:13:50 +0200

    FEAT(rgaa4): enable rule 11.2.3 in database
    rule specification:
    https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-11-2-3

--- rules/rules-rgaa4.0/src/main/resources/sql/rgaa4.0-insert.sql
+++ rules/rules-rgaa4.0/src/main/resources/sql/rgaa4.0-insert.sql
@@ -737,7 +737,7 @@ UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-2-2-1';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-1-3';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-1';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-2';
--- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-3';
+UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-3';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-4';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-5';
 -- UPDATE TEST SET `No_Process`=b'0' WHERE Cd_Test='Rgaa40-11-2-6';
