ca4c40dddf71af31df57bc855a390569517cab6f
koj <jk-dev@meskoj.fr>
2020-10-02 16:15:13 +0200

    :wrench: deploy to dev server setup

---
+++ ci/deploy_to_server.sh
@@ -0,0 +1,28 @@
+#!/bin/sh -ev
+
+SSH_KEY=${SSH_KEY?SSH_KEY must be set}
+
+eval "$(ssh-agent -s)"
+ssh-add <(echo "${SSH_KEY}")
+mkdir -p ~/.ssh
+if [[ -f /.dockerenv ]]; then
+    echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+fi
+
+CI_PROJECT_NAME=asqatasun
+USER=${USER?USER must be set}
+HOST=${HOST?HOST must be set}
+WEBAPP_ARTIFACT_DIR=${ARTIFACT_DIR?ARTIFACT_DIR must be set}
+WEBAPP_ARTIFACT_FILE=${ARTIFACT_FILE?ARTIFACT_FILE must be set}
+REST_SERVER_ARTIFACT_DIR=${ARTIFACT_DIR?ARTIFACT_DIR must be set}
+REST_SERVER_ARTIFACT_FILE=${ARTIFACT_FILE?ARTIFACT_FILE must be set}
+
+REMOTE_DIR="$CI_PROJECT_NAME/deploy-$(date +"%Y%m%d-%H%M%S")"
+ssh "${USER}"@"${HOST}" bash -c "'mkdir -p $REMOTE_DIR'"
+scp "${WEBAPP_ARTIFACT_DIR}"/"${WEBAPP_ARTIFACT_FILE}" "$USER"@"$HOST":~/"$REMOTE_DIR"
+scp "${REST_SERVER_ARTIFACT_DIR}"/"${REST_SERVER_ARTIFACT_FILE}" "$USER"@"$HOST":~/"$REMOTE_DIR"
+
+# copy docker and databases scripts and install dependencies for dev env
+
+ssh "$USER"@"$HOST" bash -c "'cd $REMOTE_DIR && APP_VERSION=$APP_VERSION CI_PROJECT_NAME=$CI_PROJECT_NAME WEBAPP_ARTIFACT_FILE=$WEBAPP_ARTIFACT_FILE REST_SERVER_ARTIFACT_FILE=$REST_SERVER_ARTIFACT_FILE ./update_app.sh'"
+ssh "$USER"@"$HOST" bash -c "'rm -rf $REMOTE_DIR'"
