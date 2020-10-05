df173692479405d540dcf62d03e67db50fb9db42
Fabrice Gangler <fabrice.gangler@adullact.org>
2020-10-05 17:57:21 +0200

    FEAT(rgaa4): import 10.4.1 from Rgaa3.0 (same ID / same content)
    rule 10.4.1: same ID and same content
    
    rule specification:
    https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-10-4-1
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule100401.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule100401Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule100401/*
    /documentation/en/90_Rules/rgaa3.0/10.Presentation_of_information/Rule-10-4-1.md

--- rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule100401.java
+++ rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule100401.java
@@ -19,7 +19,8 @@
  */
 package org.asqatasun.rules.rgaa40;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.ruleimplementation.AbstractPageRuleCssImplementation;
+import org.asqatasun.rules.csschecker.ForbiddenUnitChecker;
 
 /**
  * Implementation of rule 10.4.1 (referential RGAA 4.0)
@@ -27,13 +28,10 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about implementation, refer to <a href="https://gitlab.com/asqatasun/Asqatasun/-/blob/master/documentation/en/90_Rules/rgaa4.0/10.Presentation_of_information/Rule-10-4-1.md">rule 10.4.1 design page</a>.
  * @see <a href="https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-10-4-1">10.4.1 rule specification</a>
  */
-public class Rgaa40Rule100401 extends AbstractNotTestedRuleImplementation {
+public class Rgaa40Rule100401 extends AbstractPageRuleCssImplementation {
 
-    /**
-     * Default constructor
-     */
     public Rgaa40Rule100401() {
-        super();
+        super(new ForbiddenUnitChecker(),"MediaListNotAcceptingRelativeUnits");
     }
 
 }
