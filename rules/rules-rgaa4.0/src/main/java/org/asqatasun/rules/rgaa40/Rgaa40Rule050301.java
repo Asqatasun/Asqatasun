9e799b5bd2b69da859c3a230ce062590b5c1ecf3
Fabrice Gangler <fabrice.gangler@adullact.org>
2020-10-05 17:57:20 +0200

    FEAT(rgaa4): import 5.3.1 from Rgaa3.0 (same ID / same content)
    rule 5.3.1: same ID and same content
    
    rule specification:
    https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-5-3-1
    
    source files:
    /rules/rules-rgaa3.0/src/main/java/org/asqatasun/rules/rgaa30/Rgaa30Rule050301.java
    /rules/rules-rgaa3.0/src/test/java/org/asqatasun/rules/rgaa30/Rgaa30Rule050301Test.java
    /rules/rules-rgaa3.0/src/test/resources/testcases/rgaa30/Rgaa30Rule050301/*
    /documentation/en/90_Rules/rgaa3.0/05.Tables/Rule-5-3-1.md

--- rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule050301.java
+++ rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule050301.java
@@ -19,7 +19,25 @@
  */
 package org.asqatasun.rules.rgaa40;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.asqatasun.entity.audit.TestSolution;
+import org.asqatasun.ruleimplementation.AbstractMarkerPageRuleImplementation;
+import org.asqatasun.rules.elementchecker.CompositeChecker;
+import org.asqatasun.rules.elementchecker.attribute.AttributeWithValuePresenceChecker;
+import org.asqatasun.rules.elementchecker.element.ElementPresenceChecker;
+import org.asqatasun.rules.elementselector.SimpleElementSelector;
+import org.asqatasun.rules.keystore.AttributeStore;
+import static org.asqatasun.rules.keystore.AttributeStore.PRESENTATION_VALUE;
+import static org.asqatasun.rules.keystore.AttributeStore.ROLE_ATTR;
+import static org.asqatasun.rules.keystore.HtmlElementStore.TABLE_ELEMENT;
+import static org.asqatasun.rules.keystore.MarkerStore.COMPLEX_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.DATA_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.MarkerStore.PRESENTATION_TABLE_MARKER;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_LINEARISED_CONTENT_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_NATURE_OF_TABLE_AND_LINEARISED_CONTENT_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_TABLE_IS_NOT_PRESENTATION_WITHOUT_ROLE_ARIA_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.CHECK_TABLE_IS_PRESENTATION_WITH_ROLE_ARIA_MSG;
+import static org.asqatasun.rules.keystore.RemarkMessageStore.PRESENTATION_TABLE_WITHOUT_ARIA_MARKUP_MSG;
 
 /**
  * Implementation of rule 5.3.1 (referential RGAA 4.0)
@@ -27,13 +45,55 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about implementation, refer to <a href="https://gitlab.com/asqatasun/Asqatasun/-/blob/master/documentation/en/90_Rules/rgaa4.0/05.Tables/Rule-5-3-1.md">rule 5.3.1 design page</a>.
  * @see <a href="https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-5-3-1">5.3.1 rule specification</a>
  */
-public class Rgaa40Rule050301 extends AbstractNotTestedRuleImplementation {
+public class Rgaa40Rule050301 extends AbstractMarkerPageRuleImplementation {
 
     /**
      * Default constructor
      */
     public Rgaa40Rule050301() {
-        super();
+        super(
+                new SimpleElementSelector(TABLE_ELEMENT),
+                
+                // the presentation tables are not part of the scope
+                new String[]{PRESENTATION_TABLE_MARKER},
+                
+                // the data and complex tables are part of the scope
+                new String[]{DATA_TABLE_MARKER,COMPLEX_TABLE_MARKER},
+                
+                new CompositeChecker(
+                        new ElementPresenceChecker(
+                                // nmi when element is found
+                                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_LINEARISED_CONTENT_MSG),
+                                // na when element is not found
+                                new ImmutablePair(TestSolution.NOT_APPLICABLE,"")
+                        ),
+                        new AttributeWithValuePresenceChecker(
+                                ROLE_ATTR,
+                                PRESENTATION_VALUE,
+                                // empty msg because the CHECK_LINEARISED_CONTENT_MSG
+                                // is already use above in this case.
+                                new ImmutablePair(TestSolution.NEED_MORE_INFO,""),
+                                new ImmutablePair(TestSolution.FAILED,PRESENTATION_TABLE_WITHOUT_ARIA_MARKUP_MSG),
+                                AttributeStore.ROLE_ATTR
+                        )
+                ),
+                // checker for elements not identified by marker
+                new CompositeChecker(
+                        new ElementPresenceChecker(
+                                // nmi when element is found
+                                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_NATURE_OF_TABLE_AND_LINEARISED_CONTENT_MSG),
+                                // na when element is not found
+                                new ImmutablePair(TestSolution.NOT_APPLICABLE,"")
+                        ),
+                        new AttributeWithValuePresenceChecker(
+                                ROLE_ATTR,
+                                PRESENTATION_VALUE,
+                                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_TABLE_IS_PRESENTATION_WITH_ROLE_ARIA_MSG),
+                                new ImmutablePair(TestSolution.NEED_MORE_INFO,CHECK_TABLE_IS_NOT_PRESENTATION_WITHOUT_ROLE_ARIA_MSG),
+                                AttributeStore.ROLE_ATTR
+                        )
+                )
+        );
     }
 
 }
