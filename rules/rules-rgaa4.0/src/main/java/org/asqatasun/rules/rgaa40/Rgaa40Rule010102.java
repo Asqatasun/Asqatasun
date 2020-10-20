68e961cfab1fd1908bdc6fb49d8c2dc09ff74746
koj <jk-dev@meskoj.fr>
2020-10-20 12:16:13 +0200

    :sparkles: 1.1.2 implementation

--- rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule010102.java
+++ rules/rules-rgaa4.0/src/main/java/org/asqatasun/rules/rgaa40/Rgaa40Rule010102.java
@@ -19,7 +19,11 @@
  */
 package org.asqatasun.rules.rgaa40;
 
-import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
+import org.asqatasun.rules.elementselector.AreaElementSelector;
+import org.asqatasun.rules.elementselector.ImageElementSelector;
+
+import static org.asqatasun.rules.keystore.AttributeStore.*;
+import static org.asqatasun.rules.keystore.EvidenceStore.COMPUTED_LINK_TITLE;
 
 /**
  * Implementation of rule 1.1.2 (referential RGAA 4.0)
@@ -27,13 +31,17 @@ import org.asqatasun.ruleimplementation.AbstractNotTestedRuleImplementation;
  * For more details about implementation, refer to <a href="https://gitlab.com/asqatasun/Asqatasun/-/blob/master/documentation/en/90_Rules/rgaa4.0/01.Images/Rule-1-1-2.md">rule 1.1.2 design page</a>.
  * @see <a href="https://www.numerique.gouv.fr/publications/rgaa-accessibilite/methode/criteres/#test-1-1-2">1.1.2 rule specification</a>
  */
-public class Rgaa40Rule010102 extends AbstractNotTestedRuleImplementation {
+public class Rgaa40Rule010102 extends AbstractInformativeImagePresenceAlternativePageRuleImplementation {
 
-    /**
-     * Default constructor
-     */
     public Rgaa40Rule010102() {
-        super();
+        super(
+            new ImageElementSelector(new AreaElementSelector(), true, true),
+            true,
+            ALT_ATTR,
+            TITLE_ATTR,
+            ARIA_LABEL_ATTR,
+            COMPUTED_LINK_TITLE,
+            HREF_ATTR);
     }
 
 }
