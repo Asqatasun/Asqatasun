bf55abaf94f6493f1580b3a6d15066a6629be83d
Fabrice Gangler <fabrice.gangler@adullact.org>
2021-11-02 18:05:08 +0100

    CHORE(docker): improvement

---
+++ docker/webapp_SNAPSHOT-local/shared_download/download_prerequisites.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/78.2.0esr/linux-x86_64/en-US/firefox-78.2.0esr.tar.bz2
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
