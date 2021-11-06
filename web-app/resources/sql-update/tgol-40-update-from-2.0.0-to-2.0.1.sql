519a0a37f2d33029dc32dc658b064b379380d8d2
Fabrice Gangler <fabrice@asqatasun.org>
2021-11-06 13:17:06 +0100

    CHORE(clean up): remove obsolete SQL files
    Refs: #598

--- web-app/resources/sql-update/tgol-40-update-from-2.0.0-to-2.0.1.sql
+++
@@ -1,6 +0,0 @@
-
-INSERT IGNORE INTO `TGSI_SCOPE` (`Id_Scope`, `Code`, `Label`) VALUES
-(6, 'SCENARIO', 'Scenario');
-
-INSERT IGNORE INTO `TGSI_FUNCTIONALITY` (`Id_Functionality`, `Code`, `Label`, `Description`) VALUES
-(4, 'SCENARIO', 'Audit Scenario', '');
